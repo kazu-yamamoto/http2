6d668e4cc28b8433dd5253a87112273d061d9eab
Kazu Yamamoto <kazu@iij.ad.jp>
2013-12-04 21:36:34 +0900

    doc.

--- Network/HPACK/Huffman.hs
+++ Network/HPACK/Huffman.hs
@@ -1,10 +1,12 @@
 module Network.HPACK.Huffman (
-    encode
+  -- * Huffman encoding
+    Encoder
   , toEncoder
-  , Encoder
-  , decode
-  , toDecoder
+  , encode
+  -- * Huffman decoding
   , Decoder
+  , toDecoder
+  , decode
   ) where
 
 import Control.Arrow (second)
@@ -14,6 +16,7 @@ import Network.HPACK.Bit
 
 ----------------------------------------------------------------
 
+-- | Type for Huffman encoding.
 newtype Encoder = Encoder (Array Int Bits)
 
 idxEos :: Int
@@ -22,9 +25,11 @@ idxEos = 256
 enc :: Encoder -> Int -> Bits
 enc (Encoder ary) i = ary ! i
 
+-- | Creating 'Encoder'.
 toEncoder :: [Bits] -> Encoder
 toEncoder bs = Encoder $ listArray (0,256) bs
 
+-- | Huffman encoding.
 encode :: Encoder -> [Int] -> [Int]
 encode encoder is = map toInt $ group8 bits
   where
@@ -40,6 +45,7 @@ encode encoder is = map toInt $ group8 bits
 
 ----------------------------------------------------------------
 
+-- | Type for Huffman decoding.
 data Decoder = Tip Int | Bin Decoder Decoder deriving Show
 
 dec :: Decoder -> Bits -> (Int,Bits)
@@ -48,6 +54,7 @@ dec (Bin l _) (F:xs) = dec l xs
 dec (Bin _ r) (T:xs) = dec r xs
 dec _         []     = (-1,[])
 
+-- | Creating 'Decoder'.
 toDecoder :: [Bits] -> Decoder
 toDecoder decoder = build $ zip [0..idxEos] decoder
 
@@ -59,6 +66,7 @@ build xs = Bin (build fs) (build ts)
     fs = map (second tail) fs'
     ts = map (second tail) ts'
 
+-- | Huffman decoding.
 decode :: Decoder -> [Int] -> [Int]
 decode decoder is = decodeBits decoder (concatMap toBits is)
 
