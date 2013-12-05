a6a34c68fe925ef28b3c12f0114a34d28619b528
Kazu Yamamoto <kazu@iij.ad.jp>
2013-12-05 15:43:00 +0900

    design by types.

--- Network/HPACK.hs
+++ Network/HPACK.hs
@@ -1,18 +1,72 @@
+-- | FIXME
+
 module Network.HPACK (
-  -- * Table
-    module Network.HPACK.Table
-  -- * Context
-  , module Network.HPACK.Context
-  -- * Decoder
-  , module Network.HPACK.Decode
-  -- * Representation
-  , module Network.HPACK.Representation
-  -- * Huffman
-  , module Network.HPACK.Huffman
+  -- * Type
+    ByteStream
+  , HeaderSet
+  , EncodeContenxt
+  , DecodeContenxt
+  -- * Request
+  , encodeRequestHeader
+  , decodeRequestHeader
+  -- * Response
+  , encodeResponseHeader
+  , decodeResponseHeader
   ) where
 
-import Network.HPACK.Context
-import Network.HPACK.Decode
+import Control.Arrow (first)
 import Network.HPACK.Huffman
-import Network.HPACK.Table
-import Network.HPACK.Representation
+
+data HeaderSet
+data EncodeContenxt
+data DecodeContenxt
+
+----------------------------------------------------------------
+
+encodeRequestHeader :: HeaderSet
+                    -> EncodeContenxt
+                    -> (ByteStream, EncodeContenxt)
+encodeRequestHeader hs ctx =
+    first (toByteStream huffmanEncodingInRequest) $ toHeaderBlock hs ctx
+
+decodeRequestHeader :: ByteStream
+                    -> DecodeContenxt
+                    -> Maybe (HeaderSet, DecodeContenxt)
+decodeRequestHeader bs ctx =
+    fromHeaderBlock (fromByteStream huffmanDecodingInRequest bs) ctx
+
+----------------------------------------------------------------
+
+encodeResponseHeader :: HeaderSet
+                     -> EncodeContenxt
+                     -> (ByteStream, EncodeContenxt)
+encodeResponseHeader hs ctx =
+    first (toByteStream huffmanEncodingInResponse) $ toHeaderBlock hs ctx
+
+decodeResponseHeader :: ByteStream
+                    -> DecodeContenxt
+                    -> Maybe (HeaderSet, DecodeContenxt)
+decodeResponseHeader bs ctx =
+    fromHeaderBlock (fromByteStream huffmanDecodingInResponse bs) ctx
+
+----------------------------------------------------------------
+
+data HeaderBlock
+
+toByteStream :: HuffmanEncoding -> HeaderBlock -> ByteStream
+toByteStream = undefined
+
+fromByteStream :: HuffmanDecoding -> ByteStream -> HeaderBlock
+fromByteStream = undefined
+
+----------------------------------------------------------------
+
+toHeaderBlock :: HeaderSet
+              -> EncodeContenxt
+              -> (HeaderBlock, EncodeContenxt)
+toHeaderBlock = undefined
+
+fromHeaderBlock :: HeaderBlock
+                -> DecodeContenxt
+                -> Maybe (HeaderSet, DecodeContenxt)
+fromHeaderBlock = undefined
--- Network/HPACK/Huffman.hs
+++ Network/HPACK/Huffman.hs
@@ -1,7 +1,22 @@
-module Network.HPACK.Huffman (
-    module Network.HPACK.Huffman.Request
-  , module Network.HPACK.Huffman.Response
-  ) where
+-- | FIXME
 
-import Network.HPACK.Huffman.Request
-import Network.HPACK.Huffman.Response
+module Network.HPACK.Huffman where
+
+import Data.ByteString (ByteString)
+
+type ByteStream = ByteString
+data HeaderStuff -- ByteString
+type HuffmanEncoding = HeaderStuff -> ByteStream
+type HuffmanDecoding = ByteStream -> HeaderStuff
+
+huffmanEncodingInRequest :: HuffmanEncoding
+huffmanEncodingInRequest = undefined
+
+huffmanDecodingInRequest :: HuffmanDecoding
+huffmanDecodingInRequest = undefined
+
+huffmanEncodingInResponse :: HuffmanEncoding
+huffmanEncodingInResponse = undefined
+
+huffmanDecodingInResponse :: HuffmanDecoding
+huffmanDecodingInResponse = undefined
