f39c00e665dcd67795c658ede7802a0ff0952aa4
Kazu Yamamoto <kazu@iij.ad.jp>
2013-12-11 15:33:01 +0900

    implementing String.

--- Network/HPACK/HeaderBlock/String.hs
+++ Network/HPACK/HeaderBlock/String.hs
@@ -1,11 +1,12 @@
 module Network.HPACK.HeaderBlock.String where
 
+import qualified Data.ByteString as BS
 import Data.Word (Word8)
 import Network.HPACK.Huffman
 import Network.HPACK.Types
 
 encode :: HuffmanEncoding -> HeaderStuff -> [Word8]
-encode = undefined
+encode he h = he $ BS.unpack h
 
 decode :: HuffmanDecoding -> [Word8] -> HeaderStuff
-decode = undefined
+decode hd ws = BS.pack $ hd ws
