4e526a4e5d8c4318b238eb1f09160198178a93e3
Kazu Yamamoto <kazu@iij.ad.jp>
2013-12-12 16:53:26 +0900

    toIndexValue.

--- Network/HPACK/Table/Header.hs
+++ Network/HPACK/Table/Header.hs
@@ -5,6 +5,7 @@ module Network.HPACK.Table.Header (
   , printHeaderTable
   -- * Utilities
   , insertEntry
+  , toIndexValue
   ) where
 
 import Data.Array.IO (IOArray, newArray, readArray, writeArray)
@@ -29,12 +30,11 @@ data HeaderTable = HeaderTable {
 -- | Printing 'HeaderTable'.
 printHeaderTable :: HeaderTable -> IO ()
 printHeaderTable (HeaderTable maxN off n tbl tblsiz _) = do
-    es <- mapM (readArray tbl . adj) [beg .. end]
+    es <- mapM (readArray tbl . adj maxN) [beg .. end]
     let ts = zip [1..] es
     mapM_ printEntry ts
     putStrLn $ "      Table size: " ++ show tblsiz
   where
-    adj x = (x + maxN) `mod` maxN
     beg = off + 1
     end = off + n
 
@@ -88,7 +88,7 @@ insertOne e hdrtbl@(HeaderTable maxN off n tbl tsize _) = do
   where
     i = off
     tsize' = tsize + entrySize e
-    off' = (off - 1 + maxN) `mod` maxN
+    off' = adj maxN (off - 1)
 
 adjustTableSize :: HeaderTable -> IO (HeaderTable, [Index])
 adjustTableSize hdrtbl = adjust hdrtbl []
@@ -105,7 +105,7 @@ adjust hdrtbl is
 
 removeOne :: HeaderTable -> IO (HeaderTable,Index)
 removeOne hdrtbl@(HeaderTable maxN off n tbl tsize _) = do
-    let i = (off + n + maxN) `mod` maxN
+    let i = adj maxN (off + n)
     e <- readArray tbl i
     writeArray tbl i dummyEntry -- let the entry GCed
     let tsize' = tsize - entrySize e
@@ -114,3 +114,23 @@ removeOne hdrtbl@(HeaderTable maxN off n tbl tsize _) = do
           , headerTableSize = tsize'
           }
     return (hdrtbl',n)
+
+----------------------------------------------------------------
+
+toIndexValue :: HeaderName -> HeaderTable -> IO (Maybe (Index, HeaderValue))
+toIndexValue k (HeaderTable maxN off n tbl _ _) = go 1
+  where
+    go i
+      | i > n     = return Nothing
+      | otherwise = do
+        let idx = adj maxN (off + i)
+        e <- readArray tbl idx
+        if entryHeaderName e == k then
+            return $ Just (i, entryHeaderValue e)
+          else
+            go (i+1)
+
+----------------------------------------------------------------
+
+adj :: Int -> Int -> Int
+adj maxN x = (x + maxN) `mod` maxN
