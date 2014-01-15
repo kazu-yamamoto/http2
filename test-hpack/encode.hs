882b04753a8610fc5f48448daab87bcb87ff899b
Kazu Yamamoto <kazu@iij.ad.jp>
2014-01-15 21:50:42 +0900

    removing unnecessary imports.

--- test-hpack/encode.hs
+++ test-hpack/encode.hs
@@ -1,15 +1,8 @@
 module Main where
 
-import Control.Applicative
-import Control.Monad (mzero)
 import Data.Aeson
 import Data.Aeson.Encode.Pretty (encodePretty)
-import qualified Data.ByteString.Char8 as B8
 import qualified Data.ByteString.Lazy.Char8 as BL
-import qualified Data.Text as T
-import Data.Vector ((!))
-import qualified Data.Vector as V
-import Network.HPACK
 
 import HPACK
 import Types
