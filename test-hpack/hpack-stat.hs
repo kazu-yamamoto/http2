ba75958fe3bc67b5001a0196dffb79b6cc60b1f7
Kazu Yamamoto <kazu@iij.ad.jp>
2014-01-29 22:55:03 +0900

    hpack-stat.

---
+++ test-hpack/hpack-stat.hs
@@ -0,0 +1,95 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+
+module Main where
+
+import Control.Applicative
+import Data.Aeson
+import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy.Char8 as BL
+import Data.List
+import System.Directory
+import System.FilePath
+import Control.Monad
+
+import Types
+
+hdir :: FilePath
+hdir = "test-hpack/hpack-test-case/haskell-http2"
+
+wdir1 :: FilePath
+wdir1 = "test-hpack/hpack-test-case/haskell-http2-naive"
+
+wdir2 :: FilePath
+wdir2 = "test-hpack/hpack-test-case/haskell-http2-naive-huffman"
+
+wdir3 :: FilePath
+wdir3 = "test-hpack/hpack-test-case/haskell-http2-linear"
+
+wdir4 :: FilePath
+wdir4 = "test-hpack/hpack-test-case/haskell-http2-linear-huffman"
+
+wdir5 :: FilePath
+wdir5 = "test-hpack/hpack-test-case/haskell-http2-diff"
+
+wdir6 :: FilePath
+wdir6 = "test-hpack/hpack-test-case/haskell-http2-diff-huffman"
+
+
+main :: IO ()
+main = do
+    hs  <- get getHeaderSize hdir
+    hlen <- get getHeaderLen hdir
+    ws1 <- get getWireSize wdir1
+    ws2 <- get getWireSize wdir2
+    ws3 <- get getWireSize wdir3
+    ws4 <- get getWireSize wdir4
+    ws5 <- get getWireSize wdir5
+    ws6 <- get getWireSize wdir6
+    let h  :: Float = fromIntegral $ sum hs
+        w1 :: Float = fromIntegral $ sum ws1
+        w2 :: Float = fromIntegral $ sum ws2
+        w3 :: Float = fromIntegral $ sum ws3
+        w4 :: Float = fromIntegral $ sum ws4
+        w5 :: Float = fromIntegral $ sum ws5
+        w6 :: Float = fromIntegral $ sum ws6
+        hl :: Float = fromIntegral $ sum hlen
+    print (w1 / h
+          ,w2 / h
+          ,w3 / h
+          ,w4 / h
+          ,w5 / h
+          ,w6 / h)
+    print ((w4 - w6) / hl)
+
+get :: (FilePath -> IO Int) -> String -> IO [Int]
+get func dir = do
+    files0 <- valid . sort <$> getDirectoryContents dir
+    files1 <- filterM doesFileExist files0
+    mapM func files1
+  where
+    valid = map (dir </>) . filter (isSuffixOf ".json")
+
+getHeaderSize :: FilePath -> IO Int
+getHeaderSize file = do
+    bs <- BL.readFile file
+    let Just tc = decode bs :: Maybe Test
+    let len = sum $ map toT $ cases tc
+    return len
+  where
+    toT (Case _ _ hs _) = sum $ map (\(x,y) -> BS.length x + BS.length y) hs
+
+getHeaderLen :: FilePath -> IO Int
+getHeaderLen file = do
+    bs <- BL.readFile file
+    let Just tc = decode bs :: Maybe Test
+    let len = length $ cases tc
+    return len
+
+getWireSize :: FilePath -> IO Int
+getWireSize file = do
+    bs <- BL.readFile file
+    let Just tc = decode bs :: Maybe Test
+    let len = sum $ map toT $ cases tc
+    return len
+  where
+    toT (Case _ w _ _) = BS.length w `div` 2
