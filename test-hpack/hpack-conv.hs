69c31e9f5e3b53c6aaa0427db417ee072d373bd7
Kazu Yamamoto <kazu@iij.ad.jp>
2014-01-31 14:28:56 +0900

    adding converter.

---
+++ test-hpack/hpack-conv.hs
@@ -0,0 +1,18 @@
+module Main where
+
+import Data.Aeson
+import Data.Aeson.Encode.Pretty (encodePretty)
+import qualified Data.ByteString.Lazy.Char8 as BL
+
+import Types
+
+main :: IO ()
+main = do
+    bs <- BL.getContents
+    let Just tc = decode bs :: Maybe Test
+    let cs = cases tc
+        cs' = zipWith update cs [0..]
+        tc' = tc { cases = cs' }
+    BL.putStrLn $ encodePretty tc'
+  where
+    update c i = c { seqno = Just i }
