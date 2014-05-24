316a6f950306f476fafdbb16b4697108ae095f40
Kazu Yamamoto <kazu@iij.ad.jp>
2014-05-24 14:47:38 +0900

    removing unnecessary function.

--- Network/HPACK/HeaderBlock/String.hs
+++ Network/HPACK/HeaderBlock/String.hs
@@ -13,11 +13,6 @@ encode :: Bool -> HeaderStuff -> ByteString
 encode True h = Huffman.encode h
 encode _    h = h
 
--- fixme: removing
--- | Decoding 'ByteString' to 'HeaderStuff' according to 'HuffmanDecoding'.
-decode :: ByteString -> Either DecodeError HeaderStuff
-decode ws = Huffman.decode ws
-
 -- | Parsing 'HeaderStuff' from 'ByteString'.
 --   The second 'Bool' is whether or not huffman encoding is used.
 --   The third 'Int' is the length of the encoded string.
@@ -26,6 +21,6 @@ parseString :: Bool -> Int -> ByteString
 parseString False len bs = Right (es, bs')
   where
     (es, bs') = BS.splitAt len bs
-parseString True  len bs = decode es >>= \x -> return (x,bs')
+parseString True  len bs = Huffman.decode es >>= \x -> return (x,bs')
   where
     (es, bs') = BS.splitAt len bs
