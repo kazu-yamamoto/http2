cb8fb6a5b8ffeda4701798279dae193f19992964
Kazu Yamamoto <kazu@iij.ad.jp>
2014-09-04 16:05:10 +0900

    test for encodeFrame.

--- test/HTTP2/FrameSpec.hs
+++ test/HTTP2/FrameSpec.hs
@@ -1,15 +1,27 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module HTTP2.FrameSpec where
 
 import Test.Hspec
 
+import qualified Data.ByteString.Char8 as B
 import Network.HTTP2.Decode
 import Network.HTTP2.Encode
 import Network.HTTP2.Types
 
 spec :: Spec
 spec = do
-    describe "encodeFrameHeader" $ do
-        it "encodes frames properly" $ do
-            let frame = FrameHeader 500 FrameData 0 (StreamIdentifier 10)
-                eframe = decodeFrameHeader defaultSettings $ encodeFrameHeader frame
+    describe "encodeFrameHeader & decodeFrameHeader" $ do
+        it "encode/decodes frames properly" $ do
+            let header = FrameHeader 500 FrameData 0 (StreamIdentifier 10)
+                eheader = decodeFrameHeader defaultSettings $ encodeFrameHeader header
+            eheader `shouldBe` Right header
+
+    describe "encodeFrame & decodeFrame" $ do
+        it "encode/decodes frames properly" $ do
+            let body = "Hello, world!"
+                len = B.length body
+                header = FrameHeader len FrameData 0 (StreamIdentifier 10)
+                frame = Frame header (DataFrame body)
+                eframe = decodeFrame defaultSettings $ encodeFrame frame
             eframe `shouldBe` Right frame
