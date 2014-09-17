d460fb170aaf88c8830e97f4214276f9a526b90e
Kazu Yamamoto <kazu@iij.ad.jp>
2014-09-17 13:44:44 +0900

    implementing FrameSpec.

--- test-frame/FrameSpec.hs
+++ test-frame/FrameSpec.hs
@@ -1,6 +1,48 @@
 module FrameSpec where
 
+
+import Control.Monad (forM_)
+import Data.Aeson (eitherDecode)
+import qualified Data.ByteString.Lazy as BL
+import Data.Hex
+import Network.HTTP2
+import System.Directory (getDirectoryContents)
+import System.FilePath ((</>))
 import Test.Hspec
 
+import JSON
+import Data.List (isPrefixOf)
+
+testDir :: FilePath
+testDir = "test-frame/json"
+
+getTestFiles :: FilePath -> IO [FilePath]
+getTestFiles dir = do
+    files0 <- getDirectoryContents dir
+    let files1 = filter (not . isPrefixOf ".") files0
+        files2 = map (dir </>) files1
+    return files2
+
+check :: FilePath -> IO ()
+check file = do
+    bs <- BL.readFile file
+    let etc = eitherDecode bs :: Either String Case
+    case etc of
+        Left _ -> putStrLn $ "JSON error: " ++ file
+        Right tc -> do
+            let Just hexwire = unhex $ wire tc
+                erc = decodeFrame defaultSettings hexwire
+            case erc of
+                Left e -> do
+                    let Just errs = err tc
+                    errs `shouldContain` [fromErrorCodeId e]
+                Right frm -> do
+                    let Just fp = frame tc
+                    fpFrame fp `shouldBe` frm
+
 spec :: Spec
-spec = undefined
+spec = do
+    describe "decodeFrame and encodeFrame" $ do
+        it "decodes test cases well" $ do
+            files <- getTestFiles testDir
+            forM_ files check
