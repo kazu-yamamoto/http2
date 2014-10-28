855bd567aafa2acaad8ad118f42e73d4225240ae
Kazu Yamamoto <kazu@iij.ad.jp>
2014-10-28 11:55:59 +0900

    adding connectionPreface.

--- Network/HTTP2.hs
+++ Network/HTTP2.hs
@@ -1,11 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Network.HTTP2 (
+  -- * Magic
+    connectionPreface
   -- * Encoding and decoding
-    EncodeInfo(..)
+  , EncodeInfo(..)
   , encodeFrame
   , decodeFrame
   , module Network.HTTP2.Types
   ) where
 
+import Data.ByteString (ByteString)
 import Network.HTTP2.Decode
 import Network.HTTP2.Encode
 import Network.HTTP2.Types
+
+-- | "PRI * HTTP/2.0\\r\\n\\r\\nSM\\r\\n\\r\\n"
+connectionPreface :: ByteString
+connectionPreface = "PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n"
