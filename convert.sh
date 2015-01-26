0d5adbd461953befc1594b3b2f4c1a66704f5d9e
Kazu Yamamoto <kazu@iij.ad.jp>
2015-01-26 16:26:56 +0900

    adding a script.

---
+++ convert.sh
@@ -0,0 +1,16 @@
+#! /bin/sh
+
+set -x
+
+TOPDIR="test-frame"
+CMD="./dist/build/frame-encode/frame-encode"
+
+for i in $TOPDIR/source/*
+do
+  $CMD -w < $i > $TOPDIR/wire/`basename $i`
+done
+
+for i in $TOPDIR/wire/*
+do
+  $CMD < $i > $TOPDIR/json/`basename $i`
+done
