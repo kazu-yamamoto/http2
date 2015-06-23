925fdeb3d39dfd29852a1e8ba7f1430a73702d87
Kazu Yamamoto <kazu@iij.ad.jp>
2015-06-23 22:08:21 +0900

    for GHC 7.4.

--- Network/HTTP2/Priority.hs
+++ Network/HTTP2/Priority.hs
@@ -7,8 +7,13 @@ import Control.Applicative
 #endif
 import Control.Concurrent.STM
 import Control.Monad (when, unless)
-import qualified Data.IntMap as Map
+#if __GLASGOW_HASKELL__ >= 705
+import qualified Data.IntMap.Strict as Map
 import Data.IntMap.Strict (IntMap)
+#else
+import qualified Data.IntMap as Map
+import Data.IntMap (IntMap)
+#endif
 import Network.HTTP2.RandomSkewHeap (Heap)
 import qualified Network.HTTP2.RandomSkewHeap as Heap
 import Network.HTTP2.Types
