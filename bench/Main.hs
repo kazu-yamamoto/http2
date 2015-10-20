cabcf093cc83841af7df6819e65f48ff2282cf9c
Kazu Yamamoto <kazu@iij.ad.jp>
2015-10-20 12:32:13 +0900

    adding PSQ to benchmark.

--- bench/Main.hs
+++ bench/Main.hs
@@ -2,77 +2,143 @@
 
 module Main where
 
-import qualified ArrayOfQueue as A
 import Control.Concurrent.STM
 import Criterion.Main
-import qualified Network.HTTP2.Priority.Heap as H
-import qualified RandomSkewHeap as R
 import System.Random
+
+import qualified ArrayOfQueue as A
+--import qualified BinaryHeap as B
+import qualified Network.HTTP2.Priority.Heap as O
+import qualified PSQ as P
+import qualified RandomSkewHeap as R
+
+numOfStreams :: Int
+numOfStreams = 100
+
+numOfTrials :: Int
+numOfTrials = 10000
 
 main :: IO ()
 main = do
     gen <- getStdGen
-    let xs = take 100 $ randomRs (1,256) gen
+    let xs = take numOfStreams $ randomRs (1,256) gen
+        ss = [1,3..]
+        ys = zip ss xs
     defaultMain [
-        bench "Heap"           $ whnf bench1 xs
-      , bench "RandomSkewHeap" $ whnf bench2 xs
-      , bench "Array"          $ nfIO (bench3 xs)
+        bench "RandomSkewHeap" $ whnf benchR xs
+      , bench "Okasaki Heap"   $ whnf benchO xs
+      , bench "PSQ"            $ whnf benchP ys
+--      , bench "Binary Heap"    $ nfIO (benchB xs)
+      , bench "Array of Queue" $ nfIO (benchA xs)
       ]
 
-bench1 :: [Int] -> ()
-bench1 xs = enqdeq1 pq 10000
+----------------------------------------------------------------
+
+benchR :: [Int] -> ()
+benchR ys = enqdeqR pq numOfTrials
   where
-    !pq = create1 xs H.empty
+    !pq = createR ys R.empty
 
-create1 :: [Int] -> H.PriorityQueue Int -> H.PriorityQueue Int
-create1 [] !q = q
-create1 (x:xs) !q = create1 xs q'
-  where
-    !ent = H.newEntry x x
-    !q' = H.enqueue ent q
-
-enqdeq1 :: H.PriorityQueue Int -> Int -> ()
-enqdeq1 _ 0  = ()
-enqdeq1 q !n = case H.dequeue q of
-    Nothing -> error "enqdeq1"
-    Just (ent,q') -> let q'' = H.enqueue ent q'
-                     in enqdeq1 q'' (n - 1)
-
-bench2 :: [Int] -> ()
-bench2 xs = enqdeq2 pq 10000
-  where
-    !pq = create2 xs R.empty
-
-create2 :: [Int] -> R.PriorityQueue Int -> R.PriorityQueue Int
-create2 [] !q = q
-create2 (x:xs) !q = create2 xs q'
+createR :: [Int] -> R.PriorityQueue Int -> R.PriorityQueue Int
+createR [] !q = q
+createR (x:xs) !q = createR xs q'
   where
     !ent = R.newEntry x x
     !q' = R.enqueue ent q
 
-enqdeq2 :: R.PriorityQueue Int -> Int -> ()
-enqdeq2 _ 0  = ()
-enqdeq2 q !n = case R.dequeue q of
-    Nothing -> error "enqdeq2"
-    Just (ent,q') -> let q'' = R.enqueue ent q'
-                     in enqdeq2 q'' (n - 1)
+enqdeqR :: R.PriorityQueue Int -> Int -> ()
+enqdeqR _ 0  = ()
+enqdeqR q !n = case R.dequeue q of
+    Nothing -> error "enqdeqR"
+    Just (ent,q') -> let !q'' = R.enqueue ent q'
+                       in enqdeqR q'' (n - 1)
 
-bench3 :: [Int] -> IO ()
-bench3 xs = do
+----------------------------------------------------------------
+
+benchO :: [Int] -> ()
+benchO xs = enqdeqO pq numOfTrials
+  where
+    !pq = createO xs O.empty
+
+createO :: [Int] -> O.PriorityQueue Int -> O.PriorityQueue Int
+createO [] !q = q
+createO (x:xs) !q = createO xs q'
+  where
+    !ent = O.newEntry x x
+    !q' = O.enqueue ent q
+
+enqdeqO :: O.PriorityQueue Int -> Int -> ()
+enqdeqO _ 0  = ()
+enqdeqO q !n = case O.dequeue q of
+    Nothing -> error "enqdeqO"
+    Just (ent,q') -> let !q'' = O.enqueue ent q'
+                     in enqdeqO q'' (n - 1)
+
+----------------------------------------------------------------
+
+benchP :: [(Int,Int)] -> ()
+benchP xs = enqdeqP pq numOfTrials
+  where
+    !pq = createP xs P.empty
+
+createP :: [(Int,Int)] -> P.PriorityQueue Int -> P.PriorityQueue Int
+createP [] !q = q
+createP ((k,x):xs) !q = createP xs q'
+  where
+    !ent = P.newEntry x x
+    !q' = P.enqueue k ent q
+
+enqdeqP :: P.PriorityQueue Int -> Int -> ()
+enqdeqP _ 0  = ()
+enqdeqP q !n = case P.dequeue q of
+    Nothing -> error "enqdeqP"
+    Just (k,ent,q') -> let !q'' = P.enqueue k ent q'
+                       in enqdeqP q'' (n - 1)
+
+----------------------------------------------------------------
+
+{-
+benchB :: [Int] -> IO ()
+benchB xs = do
+    q <- atomically (B.new numOfStreams)
+    createB xs q
+    enqdeqB q numOfTrials
+
+createB :: [Int] -> B.PriorityQueue Int -> IO ()
+createB [] _      = return ()
+createB (x:xs) !q = do
+    let !ent = B.newEntry x x
+    atomically $ B.enqueue ent q
+    createB xs q
+
+enqdeqB :: B.PriorityQueue Int -> Int -> IO ()
+enqdeqB _ 0  = return ()
+enqdeqB q !n = do
+    ent <- atomically $ B.dequeue q
+    atomically $ B.enqueue ent q
+    enqdeqB q (n - 1)
+-}
+
+----------------------------------------------------------------
+
+benchA :: [Int] -> IO ()
+benchA xs = do
     q <- atomically A.new
-    create3 xs q
-    enqdeq3 q 10000
+    createA xs q
+    enqdeqA q numOfTrials
 
-create3 :: [Int] -> A.PriorityQueue Int -> IO ()
-create3 [] _      = return ()
-create3 (x:xs) !q = do
+createA :: [Int] -> A.PriorityQueue Int -> IO ()
+createA [] _      = return ()
+createA (x:xs) !q = do
     let !ent = A.newEntry x x
     atomically $ A.enqueue ent q
-    create3 xs q
+    createA xs q
 
-enqdeq3 :: A.PriorityQueue Int -> Int -> IO ()
-enqdeq3 _ 0  = return ()
-enqdeq3 q !n = do
+enqdeqA :: A.PriorityQueue Int -> Int -> IO ()
+enqdeqA _ 0  = return ()
+enqdeqA q !n = do
     ent <- atomically $ A.dequeue q
     atomically $ A.enqueue ent q
-    enqdeq3 q (n - 1)
+    enqdeqA q (n - 1)
+
+----------------------------------------------------------------
---
+++ bench/PSQ.hs
@@ -0,0 +1,100 @@
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE RecordWildCards #-}
+
+module PSQ (
+    Entry
+  , newEntry
+  , renewEntry
+  , item
+  , PriorityQueue
+  , empty
+  , isEmpty
+  , enqueue
+  , dequeue
+  ) where
+
+#if __GLASGOW_HASKELL__ < 709
+import Control.Applicative ((<$>))
+#endif
+import Data.Array (Array, listArray, (!))
+import Data.IntPSQ (IntPSQ)
+import qualified Data.IntPSQ as P
+import Data.Word (Word64)
+
+----------------------------------------------------------------
+
+type Key = Int
+type Weight = Int
+type Deficit = Word64
+type Heap = IntPSQ Deficit
+
+-- | Abstract data type of entries for priority queues.
+data Entry a = Entry {
+    item :: a -- ^ Extracting an item from an entry.
+  , weight  :: {-# UNPACK #-} !Weight
+  , deficit :: {-# UNPACK #-} !Deficit
+  } deriving Show
+
+instance Eq (Entry a) where
+    Entry _ _ p1 == Entry _ _ p2 = p1 == p2
+
+instance Ord (Entry a) where
+    Entry _ _ p1 <  Entry _ _ p2 = p1 <  p2
+    Entry _ _ p1 <= Entry _ _ p2 = p1 <= p2
+
+-- FIXME: The base (Word64) would be overflowed.
+--        In that case, the heap must be re-constructed.
+data PriorityQueue a = PriorityQueue {-# UNPACK #-} !Deficit (Heap (Entry a))
+
+----------------------------------------------------------------
+
+magicDeficit :: Deficit
+magicDeficit = 0
+
+deficitSteps :: Int
+deficitSteps = 65536
+
+deficitList :: [Deficit]
+deficitList = map calc idxs
+  where
+    idxs = [1..256] :: [Double]
+    calc w = round (fromIntegral deficitSteps / w)
+
+deficitTable :: Array Int Deficit
+deficitTable = listArray (1,256) deficitList
+
+weightToDeficit :: Weight -> Deficit
+weightToDeficit w = deficitTable ! w
+
+----------------------------------------------------------------
+
+newEntry :: a -> Weight -> Entry a
+newEntry x w = Entry x w magicDeficit
+
+-- | Changing the item of an entry.
+renewEntry :: Entry a -> b -> Entry b
+renewEntry (Entry _ w p) x = Entry x w p
+
+----------------------------------------------------------------
+
+empty :: PriorityQueue a
+empty = PriorityQueue 0 P.empty
+
+isEmpty :: PriorityQueue a -> Bool
+isEmpty (PriorityQueue _ h) = P.null h
+
+enqueue :: Key -> Entry a -> PriorityQueue a -> PriorityQueue a
+enqueue k Entry{..} (PriorityQueue base heap) = PriorityQueue base heap'
+  where
+    !b = if deficit == magicDeficit then base else deficit
+    !deficit' = b + weightToDeficit weight
+    !ent' = Entry item weight deficit'
+    !heap' = P.insert k deficit' ent' heap
+
+dequeue :: PriorityQueue a -> Maybe (Key, Entry a, PriorityQueue a)
+dequeue (PriorityQueue _ heap) = case P.minView heap of
+    Nothing                     -> Nothing
+    Just (k, deficit, ent, heap')
+      | P.null heap' -> Just (k, ent, empty) -- reset the deficit base
+      | otherwise    -> Just (k, ent, PriorityQueue deficit heap')
