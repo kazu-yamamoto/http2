bdd63c1b1da8997da7cf6b5f94809f6533960b8d
Kazu Yamamoto <kazu@iij.ad.jp>
2016-01-18 12:57:14 +0900

    adding test.

--- test/HTTP2/PrioritySpec.hs
+++ test/HTTP2/PrioritySpec.hs
@@ -23,6 +23,19 @@ spec = do
                     P.enqueue 1 (P.newPrecedence 201) 1 P.empty
                 xs = enqdeq q 1000
             map length (group (sort xs)) `shouldBe` [664,333,3]
+        it "deletes properly" $ do
+            let q = P.enqueue 7 (P.newPrecedence   1) 7 $
+                    P.enqueue 5 (P.newPrecedence   5) 5 $
+                    P.enqueue 3 (P.newPrecedence  50) 3 $
+                    P.enqueue 1 (P.newPrecedence 201) (1 :: Int) P.empty
+            let Just (k1,_,_,q1) = P.dequeue q
+            k1 `shouldBe` 1
+            let (mk, q2) = P.delete 5 q1
+            mk `shouldBe` Just 5
+            let Just (k3,_,_,q3) = P.dequeue q2
+            k3 `shouldBe` 3
+            let Just (k4,_,_,_) = P.dequeue q3
+            k4 `shouldBe` 7
         it "deletes entries properly" $ do
             let q = P.enqueue 5 (P.newPrecedence   1) 5 $
                     P.enqueue 3 (P.newPrecedence 101) 3 $
