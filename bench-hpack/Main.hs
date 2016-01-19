4b2ff4f900389b4e3cfb8340e4c3357cfabde28c
Kazu Yamamoto <kazu@iij.ad.jp>
2016-01-19 16:14:08 +0900

    benchmark for HPACK decoding.

--- bench-hpack/Main.hs
+++ bench-hpack/Main.hs
@@ -5,18 +5,32 @@ module Main where
 import Control.Exception
 import Criterion.Main
 import Network.HPACK
+import Data.ByteString (ByteString)
 
 main :: IO ()
 main = do
     hdrs <- read <$> readFile "bench-hpack/headers.hs"
-    _ <- evaluate hdrs
+    hpacks <- prepare hdrs
+    _ <- evaluate hpacks
     defaultMain [
         bgroup "HPACK encoding" [
               bench "Pure" $ nfIO (enc hdrs)
             ]
+      , bgroup "HPACK decoding" [
+              bench "Pure" $ nfIO (dec hpacks)
+            ]
       ]
 
 ----------------------------------------------------------------
+prepare :: [HeaderList] -> IO [ByteString]
+prepare hdrs = do
+    tbl <- newDynamicTableForEncoding defaultDynamicTableSize
+    go tbl hdrs id
+  where
+    go _    []     b = return (b [])
+    go !tbl (h:hs) b = do
+        (tbl', frag) <- encodeHeader defaultEncodeStrategy tbl h
+        go tbl' hs (b . (frag :))
 
 enc :: [HeaderList] -> IO ()
 enc hdrs = do
@@ -27,4 +41,14 @@ enc hdrs = do
     go !tbl (h:hs) = do
         (tbl', !_) <- encodeHeader defaultEncodeStrategy tbl h
         go tbl' hs
+
+dec :: [ByteString] -> IO ()
+dec hpacks = do
+    tbl <- newDynamicTableForDecoding defaultDynamicTableSize
+    go tbl hpacks
+  where
+    go _    []     = return ()
+    go !tbl (f:fs) = do
+        (tbl', !_) <- decodeHeader tbl f
+        go tbl' fs
 
