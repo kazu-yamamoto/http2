fa0696fec0c27b7cd820586a02685a0a8dac59f0
Kazu Yamamoto <kazu@iij.ad.jp>
2016-02-08 14:43:16 +0900

    from 4 bit steps to 8 bit steps in Huffman decoding.

--- Network/HPACK2/Huffman/Decode.hs
+++ Network/HPACK2/Huffman/Decode.hs
@@ -9,7 +9,6 @@ module Network.HPACK2.Huffman.Decode (
 
 import Control.Exception (throwIO)
 import Data.Array (Array, (!), listArray)
-import Data.Bits (shiftR, (.&.))
 import Data.ByteString.Internal (ByteString(..))
 import Data.Word (Word8)
 import Network.HPACK2.Buffer
@@ -30,13 +29,16 @@ data Pin = EndOfString
          | Forward {-# UNPACK #-} !Word8 -- node no.
          | GoBack  {-# UNPACK #-} !Word8 -- node no.
                    {-# UNPACK #-} !Word8 -- a decoded value
+         | GoBack2 {-# UNPACK #-} !Word8 -- node no.
+                   {-# UNPACK #-} !Word8 -- a decoded value
+                   {-# UNPACK #-} !Word8 -- a decoded value
          deriving Show
 
-data Way16 = Way16 !(Maybe Int) !(Array Word8 Pin)
-type Way256 = Array Word8 Way16
+data WayStep = WayStep !(Maybe Int) !(Array Word8 Pin)
+type Way256 = Array Word8 WayStep
 
-next :: Way16 -> Word8 -> Pin
-next (Way16 _ a16) w = a16 ! w
+next :: WayStep -> Word8 -> Pin
+next (WayStep _ a16) w = a16 ! w
 
 ----------------------------------------------------------------
 
@@ -50,22 +52,23 @@ dec :: WorkingBuffer -> HuffmanDecoding
 dec tmp len rbuf = go len (way256 ! 0)
   where
     go 0 way0 = case way0 of
-        Way16 Nothing  _ -> throwIO IllegalEos
-        Way16 (Just i) _
+        WayStep Nothing  _ -> throwIO IllegalEos
+        WayStep (Just i) _
           | i <= 8       -> toByteString tmp
           | otherwise    -> throwIO TooLongEos
     go !n !way0 = do
         w <- getByte rbuf
-        let w0 = w `shiftR` 4
-            w1 = w .&. 0x0f
-        way1 <- doit way0 w0
-        way2 <- doit way1 w1
-        go (n - 1) way2
+        way <- doit way0 w
+        go (n - 1) way
     doit !way !w = case next way w of
         EndOfString -> throwIO EosInTheMiddle
         Forward n   -> return $ way256 ! n
         GoBack  n v -> do
             writeWord8 tmp v
+            return $ way256 ! n
+        GoBack2 n v1 v2 -> do
+            writeWord8 tmp v1
+            writeWord8 tmp v2
             return $ way256 ! n
 
 -- | Huffman decoding.
@@ -80,37 +83,289 @@ way256 = construct $ toHTree huffmanTable
 construct :: HTree -> Way256
 construct decoder = listArray (0,255) $ map to16ways $ flatten decoder
   where
-    to16ways x = Way16 ei a16
+    to16ways x = WayStep ei a16
       where
         !ei = eosInfo x
-        !a16 = listArray (0,15) $ map (step decoder x Nothing) bits4s
+        !a16 = listArray (0,255) $ map (step decoder x Non) bits8s
 
-step :: HTree -> HTree -> Maybe Word8 -> [B] -> Pin
-step root (Tip _ v)     _  bss
+data Chara = Non
+           | One !Word8
+           | Two !Word8 !Word8
+
+inc :: Chara -> Word8 -> Chara
+inc Non w     = One w
+inc (One v) w = Two v w
+inc _       _ = error "inc"
+
+step :: HTree -> HTree -> Chara -> [B] -> Pin
+step root (Tip _ v)     x  bss
   | v == idxEos                     = EndOfString
-  | otherwise                       = let w = fromIntegral v
-                                      in step root root (Just w) bss
-step _    (Bin _ n _ _) Nothing  [] = Forward (fromIntegral n)
-step _    (Bin _ n _ _) (Just w) [] = GoBack (fromIntegral n) w
-step root (Bin _ _ l _) mx   (F:bs) = step root l mx bs
-step root (Bin _ _ _ r) mx   (T:bs) = step root r mx bs
+  | otherwise                       = let !w = fromIntegral v
+                                          !x' = inc x w
+                                      in step root root x' bss
+step _    (Bin _ n _ _) Non       [] = Forward (fromIntegral n)
+step _    (Bin _ n _ _) (One w)   [] = GoBack (fromIntegral n) w
+step _    (Bin _ n _ _) (Two w z) [] = GoBack2 (fromIntegral n) w z
+step root (Bin _ _ l _) mx    (F:bs) = step root l mx bs
+step root (Bin _ _ _ r) mx    (T:bs) = step root r mx bs
 
-bits4s :: [[B]]
-bits4s = [
-    [F,F,F,F]
-  , [F,F,F,T]
-  , [F,F,T,F]
-  , [F,F,T,T]
-  , [F,T,F,F]
-  , [F,T,F,T]
-  , [F,T,T,F]
-  , [F,T,T,T]
-  , [T,F,F,F]
-  , [T,F,F,T]
-  , [T,F,T,F]
-  , [T,F,T,T]
-  , [T,T,F,F]
-  , [T,T,F,T]
-  , [T,T,T,F]
-  , [T,T,T,T]
+bits8s :: [[B]]
+bits8s = [
+    [F,F,F,F,F,F,F,F]
+  , [F,F,F,F,F,F,F,T]
+  , [F,F,F,F,F,F,T,F]
+  , [F,F,F,F,F,F,T,T]
+  , [F,F,F,F,F,T,F,F]
+  , [F,F,F,F,F,T,F,T]
+  , [F,F,F,F,F,T,T,F]
+  , [F,F,F,F,F,T,T,T]
+  , [F,F,F,F,T,F,F,F]
+  , [F,F,F,F,T,F,F,T]
+  , [F,F,F,F,T,F,T,F]
+  , [F,F,F,F,T,F,T,T]
+  , [F,F,F,F,T,T,F,F]
+  , [F,F,F,F,T,T,F,T]
+  , [F,F,F,F,T,T,T,F]
+  , [F,F,F,F,T,T,T,T]
+  , [F,F,F,T,F,F,F,F]
+  , [F,F,F,T,F,F,F,T]
+  , [F,F,F,T,F,F,T,F]
+  , [F,F,F,T,F,F,T,T]
+  , [F,F,F,T,F,T,F,F]
+  , [F,F,F,T,F,T,F,T]
+  , [F,F,F,T,F,T,T,F]
+  , [F,F,F,T,F,T,T,T]
+  , [F,F,F,T,T,F,F,F]
+  , [F,F,F,T,T,F,F,T]
+  , [F,F,F,T,T,F,T,F]
+  , [F,F,F,T,T,F,T,T]
+  , [F,F,F,T,T,T,F,F]
+  , [F,F,F,T,T,T,F,T]
+  , [F,F,F,T,T,T,T,F]
+  , [F,F,F,T,T,T,T,T]
+  , [F,F,T,F,F,F,F,F]
+  , [F,F,T,F,F,F,F,T]
+  , [F,F,T,F,F,F,T,F]
+  , [F,F,T,F,F,F,T,T]
+  , [F,F,T,F,F,T,F,F]
+  , [F,F,T,F,F,T,F,T]
+  , [F,F,T,F,F,T,T,F]
+  , [F,F,T,F,F,T,T,T]
+  , [F,F,T,F,T,F,F,F]
+  , [F,F,T,F,T,F,F,T]
+  , [F,F,T,F,T,F,T,F]
+  , [F,F,T,F,T,F,T,T]
+  , [F,F,T,F,T,T,F,F]
+  , [F,F,T,F,T,T,F,T]
+  , [F,F,T,F,T,T,T,F]
+  , [F,F,T,F,T,T,T,T]
+  , [F,F,T,T,F,F,F,F]
+  , [F,F,T,T,F,F,F,T]
+  , [F,F,T,T,F,F,T,F]
+  , [F,F,T,T,F,F,T,T]
+  , [F,F,T,T,F,T,F,F]
+  , [F,F,T,T,F,T,F,T]
+  , [F,F,T,T,F,T,T,F]
+  , [F,F,T,T,F,T,T,T]
+  , [F,F,T,T,T,F,F,F]
+  , [F,F,T,T,T,F,F,T]
+  , [F,F,T,T,T,F,T,F]
+  , [F,F,T,T,T,F,T,T]
+  , [F,F,T,T,T,T,F,F]
+  , [F,F,T,T,T,T,F,T]
+  , [F,F,T,T,T,T,T,F]
+  , [F,F,T,T,T,T,T,T]
+  , [F,T,F,F,F,F,F,F]
+  , [F,T,F,F,F,F,F,T]
+  , [F,T,F,F,F,F,T,F]
+  , [F,T,F,F,F,F,T,T]
+  , [F,T,F,F,F,T,F,F]
+  , [F,T,F,F,F,T,F,T]
+  , [F,T,F,F,F,T,T,F]
+  , [F,T,F,F,F,T,T,T]
+  , [F,T,F,F,T,F,F,F]
+  , [F,T,F,F,T,F,F,T]
+  , [F,T,F,F,T,F,T,F]
+  , [F,T,F,F,T,F,T,T]
+  , [F,T,F,F,T,T,F,F]
+  , [F,T,F,F,T,T,F,T]
+  , [F,T,F,F,T,T,T,F]
+  , [F,T,F,F,T,T,T,T]
+  , [F,T,F,T,F,F,F,F]
+  , [F,T,F,T,F,F,F,T]
+  , [F,T,F,T,F,F,T,F]
+  , [F,T,F,T,F,F,T,T]
+  , [F,T,F,T,F,T,F,F]
+  , [F,T,F,T,F,T,F,T]
+  , [F,T,F,T,F,T,T,F]
+  , [F,T,F,T,F,T,T,T]
+  , [F,T,F,T,T,F,F,F]
+  , [F,T,F,T,T,F,F,T]
+  , [F,T,F,T,T,F,T,F]
+  , [F,T,F,T,T,F,T,T]
+  , [F,T,F,T,T,T,F,F]
+  , [F,T,F,T,T,T,F,T]
+  , [F,T,F,T,T,T,T,F]
+  , [F,T,F,T,T,T,T,T]
+  , [F,T,T,F,F,F,F,F]
+  , [F,T,T,F,F,F,F,T]
+  , [F,T,T,F,F,F,T,F]
+  , [F,T,T,F,F,F,T,T]
+  , [F,T,T,F,F,T,F,F]
+  , [F,T,T,F,F,T,F,T]
+  , [F,T,T,F,F,T,T,F]
+  , [F,T,T,F,F,T,T,T]
+  , [F,T,T,F,T,F,F,F]
+  , [F,T,T,F,T,F,F,T]
+  , [F,T,T,F,T,F,T,F]
+  , [F,T,T,F,T,F,T,T]
+  , [F,T,T,F,T,T,F,F]
+  , [F,T,T,F,T,T,F,T]
+  , [F,T,T,F,T,T,T,F]
+  , [F,T,T,F,T,T,T,T]
+  , [F,T,T,T,F,F,F,F]
+  , [F,T,T,T,F,F,F,T]
+  , [F,T,T,T,F,F,T,F]
+  , [F,T,T,T,F,F,T,T]
+  , [F,T,T,T,F,T,F,F]
+  , [F,T,T,T,F,T,F,T]
+  , [F,T,T,T,F,T,T,F]
+  , [F,T,T,T,F,T,T,T]
+  , [F,T,T,T,T,F,F,F]
+  , [F,T,T,T,T,F,F,T]
+  , [F,T,T,T,T,F,T,F]
+  , [F,T,T,T,T,F,T,T]
+  , [F,T,T,T,T,T,F,F]
+  , [F,T,T,T,T,T,F,T]
+  , [F,T,T,T,T,T,T,F]
+  , [F,T,T,T,T,T,T,T]
+  , [T,F,F,F,F,F,F,F]
+  , [T,F,F,F,F,F,F,T]
+  , [T,F,F,F,F,F,T,F]
+  , [T,F,F,F,F,F,T,T]
+  , [T,F,F,F,F,T,F,F]
+  , [T,F,F,F,F,T,F,T]
+  , [T,F,F,F,F,T,T,F]
+  , [T,F,F,F,F,T,T,T]
+  , [T,F,F,F,T,F,F,F]
+  , [T,F,F,F,T,F,F,T]
+  , [T,F,F,F,T,F,T,F]
+  , [T,F,F,F,T,F,T,T]
+  , [T,F,F,F,T,T,F,F]
+  , [T,F,F,F,T,T,F,T]
+  , [T,F,F,F,T,T,T,F]
+  , [T,F,F,F,T,T,T,T]
+  , [T,F,F,T,F,F,F,F]
+  , [T,F,F,T,F,F,F,T]
+  , [T,F,F,T,F,F,T,F]
+  , [T,F,F,T,F,F,T,T]
+  , [T,F,F,T,F,T,F,F]
+  , [T,F,F,T,F,T,F,T]
+  , [T,F,F,T,F,T,T,F]
+  , [T,F,F,T,F,T,T,T]
+  , [T,F,F,T,T,F,F,F]
+  , [T,F,F,T,T,F,F,T]
+  , [T,F,F,T,T,F,T,F]
+  , [T,F,F,T,T,F,T,T]
+  , [T,F,F,T,T,T,F,F]
+  , [T,F,F,T,T,T,F,T]
+  , [T,F,F,T,T,T,T,F]
+  , [T,F,F,T,T,T,T,T]
+  , [T,F,T,F,F,F,F,F]
+  , [T,F,T,F,F,F,F,T]
+  , [T,F,T,F,F,F,T,F]
+  , [T,F,T,F,F,F,T,T]
+  , [T,F,T,F,F,T,F,F]
+  , [T,F,T,F,F,T,F,T]
+  , [T,F,T,F,F,T,T,F]
+  , [T,F,T,F,F,T,T,T]
+  , [T,F,T,F,T,F,F,F]
+  , [T,F,T,F,T,F,F,T]
+  , [T,F,T,F,T,F,T,F]
+  , [T,F,T,F,T,F,T,T]
+  , [T,F,T,F,T,T,F,F]
+  , [T,F,T,F,T,T,F,T]
+  , [T,F,T,F,T,T,T,F]
+  , [T,F,T,F,T,T,T,T]
+  , [T,F,T,T,F,F,F,F]
+  , [T,F,T,T,F,F,F,T]
+  , [T,F,T,T,F,F,T,F]
+  , [T,F,T,T,F,F,T,T]
+  , [T,F,T,T,F,T,F,F]
+  , [T,F,T,T,F,T,F,T]
+  , [T,F,T,T,F,T,T,F]
+  , [T,F,T,T,F,T,T,T]
+  , [T,F,T,T,T,F,F,F]
+  , [T,F,T,T,T,F,F,T]
+  , [T,F,T,T,T,F,T,F]
+  , [T,F,T,T,T,F,T,T]
+  , [T,F,T,T,T,T,F,F]
+  , [T,F,T,T,T,T,F,T]
+  , [T,F,T,T,T,T,T,F]
+  , [T,F,T,T,T,T,T,T]
+  , [T,T,F,F,F,F,F,F]
+  , [T,T,F,F,F,F,F,T]
+  , [T,T,F,F,F,F,T,F]
+  , [T,T,F,F,F,F,T,T]
+  , [T,T,F,F,F,T,F,F]
+  , [T,T,F,F,F,T,F,T]
+  , [T,T,F,F,F,T,T,F]
+  , [T,T,F,F,F,T,T,T]
+  , [T,T,F,F,T,F,F,F]
+  , [T,T,F,F,T,F,F,T]
+  , [T,T,F,F,T,F,T,F]
+  , [T,T,F,F,T,F,T,T]
+  , [T,T,F,F,T,T,F,F]
+  , [T,T,F,F,T,T,F,T]
+  , [T,T,F,F,T,T,T,F]
+  , [T,T,F,F,T,T,T,T]
+  , [T,T,F,T,F,F,F,F]
+  , [T,T,F,T,F,F,F,T]
+  , [T,T,F,T,F,F,T,F]
+  , [T,T,F,T,F,F,T,T]
+  , [T,T,F,T,F,T,F,F]
+  , [T,T,F,T,F,T,F,T]
+  , [T,T,F,T,F,T,T,F]
+  , [T,T,F,T,F,T,T,T]
+  , [T,T,F,T,T,F,F,F]
+  , [T,T,F,T,T,F,F,T]
+  , [T,T,F,T,T,F,T,F]
+  , [T,T,F,T,T,F,T,T]
+  , [T,T,F,T,T,T,F,F]
+  , [T,T,F,T,T,T,F,T]
+  , [T,T,F,T,T,T,T,F]
+  , [T,T,F,T,T,T,T,T]
+  , [T,T,T,F,F,F,F,F]
+  , [T,T,T,F,F,F,F,T]
+  , [T,T,T,F,F,F,T,F]
+  , [T,T,T,F,F,F,T,T]
+  , [T,T,T,F,F,T,F,F]
+  , [T,T,T,F,F,T,F,T]
+  , [T,T,T,F,F,T,T,F]
+  , [T,T,T,F,F,T,T,T]
+  , [T,T,T,F,T,F,F,F]
+  , [T,T,T,F,T,F,F,T]
+  , [T,T,T,F,T,F,T,F]
+  , [T,T,T,F,T,F,T,T]
+  , [T,T,T,F,T,T,F,F]
+  , [T,T,T,F,T,T,F,T]
+  , [T,T,T,F,T,T,T,F]
+  , [T,T,T,F,T,T,T,T]
+  , [T,T,T,T,F,F,F,F]
+  , [T,T,T,T,F,F,F,T]
+  , [T,T,T,T,F,F,T,F]
+  , [T,T,T,T,F,F,T,T]
+  , [T,T,T,T,F,T,F,F]
+  , [T,T,T,T,F,T,F,T]
+  , [T,T,T,T,F,T,T,F]
+  , [T,T,T,T,F,T,T,T]
+  , [T,T,T,T,T,F,F,F]
+  , [T,T,T,T,T,F,F,T]
+  , [T,T,T,T,T,F,T,F]
+  , [T,T,T,T,T,F,T,T]
+  , [T,T,T,T,T,T,F,F]
+  , [T,T,T,T,T,T,F,T]
+  , [T,T,T,T,T,T,T,F]
+  , [T,T,T,T,T,T,T,T]
   ]
+
