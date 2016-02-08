af9bc5910022c955c42c0dda8b6ec780d3ba3085
Kazu Yamamoto <kazu@iij.ad.jp>
2016-02-08 16:42:51 +0900

    updating bench.

--- bench-hpack/Main.hs
+++ bench-hpack/Main.hs
@@ -5,25 +5,19 @@ module Main where
 import Control.Exception
 import Criterion.Main
 import Network.HPACK
-import qualified Network.HPACK2 as N
 import Data.ByteString (ByteString)
-import Foreign.Marshal.Alloc
 
 main :: IO ()
 main = do
     hdrs <- read <$> readFile "bench-hpack/headers.hs"
     hpacks <- prepare hdrs
     _ <- evaluate hpacks
-    let bufsiz = 4096
-    buf <- mallocBytes bufsiz
     defaultMain [
         bgroup "HPACK encoding" [
-              bench "Pure" $ nfIO (enc hdrs)
-            , bench "New"  $ nfIO (enc2 hdrs)
+              bench "New"  $ nfIO (enc hdrs)
             ]
       , bgroup "HPACK decoding" [
-              bench "Pure" $ nfIO (dec hpacks)
-            , bench "New"  $ nfIO (dec2 hpacks buf bufsiz)
+              bench "New"  $ nfIO (dec hpacks)
             ]
       ]
 
@@ -35,8 +29,8 @@ prepare hdrs = do
   where
     go _    []     b = return (b [])
     go !tbl (h:hs) b = do
-        (tbl', frag) <- encodeHeader defaultEncodeStrategy tbl h
-        go tbl' hs (b . (frag :))
+        !frag <- encodeHeader defaultEncodeStrategy tbl h
+        go tbl hs (b . (frag :))
 
 enc :: [HeaderList] -> IO ()
 enc hdrs = do
@@ -45,8 +39,8 @@ enc hdrs = do
   where
     go _    []     = return ()
     go !tbl (h:hs) = do
-        (tbl', !_) <- encodeHeader defaultEncodeStrategy tbl h
-        go tbl' hs
+        !_ <- encodeHeader defaultEncodeStrategy { compressionAlgo = Linear, useHuffman = True }  tbl h
+        go tbl hs
 
 dec :: [ByteString] -> IO ()
 dec hpacks = do
@@ -55,25 +49,5 @@ dec hpacks = do
   where
     go _    []     = return ()
     go !tbl (f:fs) = do
-        (tbl', !_) <- decodeHeader tbl f
-        go tbl' fs
-
-enc2 :: [HeaderList] -> IO ()
-enc2 hdrs = do
-    tbl <- N.newDynamicTableForEncoding N.defaultDynamicTableSize
-    go tbl hdrs
-  where
-    go _    []     = return ()
-    go !tbl (h:hs) = do
-        !_ <- N.encodeHeader N.defaultEncodeStrategy tbl h
-        go tbl hs
-
-dec2 :: [ByteString] -> N.Buffer -> N.BufferSize -> IO ()
-dec2 hpacks buf siz = do
-    tbl <- N.newDynamicTableForDecoding N.defaultDynamicTableSize
-    go tbl hpacks
-  where
-    go _    []     = return ()
-    go !tbl (f:fs) = do
-        !_ <- N.decodeHeader tbl f buf siz
+        !_ <- decodeHeader tbl f
         go tbl fs
