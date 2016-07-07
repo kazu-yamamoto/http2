bf049db3f93ea1fdd7ae4892c68755b8b38ec747
Kazu Yamamoto <kazu@iij.ad.jp>
2016-07-07 17:08:54 +0900

    removing memcpy.

--- Network/HPACK/Huffman/Encode.hs
+++ Network/HPACK/Huffman/Encode.hs
@@ -13,8 +13,7 @@ import Control.Applicative ((<$>))
 import Control.Monad (when, void)
 import Data.Array
 import Data.Bits ((.|.))
-import qualified Data.ByteString as BS
-import Data.ByteString.Internal (ByteString(..))
+import Data.ByteString (ByteString)
 import Data.Word (Word8)
 import Network.HPACK.Buffer
 import Network.HPACK.Huffman.Bit
@@ -27,9 +26,16 @@ type AOSA = Array Int ShiftedArray
 
 type ShiftedArray = Array Int Shifted
 
-data Shifted = Shifted !Int        -- How many bits in the last byte
-                       !Word8      -- First word. If Int is 0, this is dummy
-                       !ByteString -- Following words, up to 4 bytes
+data WS = W0
+        | W1 !Word8
+        | W2 !Word8 !Word8
+        | W3 !Word8 !Word8 !Word8
+        | W4 !Word8 !Word8 !Word8 !Word8
+        deriving Show
+
+data Shifted = Shifted !Int   -- How many bits in the last byte
+                       !Word8 -- First word. If Int is 0, this is dummy
+                       !WS    -- Following words, up to 4 bytes
                        deriving Show
 
 ----------------------------------------------------------------
@@ -40,20 +46,27 @@ aosa = listArray (0,idxEos) $ map toShiftedArray huffmanTable
 -- |
 --
 -- >>> toShifted [T,T,T,T] 0
--- Shifted 4 240 ""
+-- Shifted 4 240 W0
 -- >>> toShifted [T,T,T,T] 4
--- Shifted 0 15 ""
+-- Shifted 0 15 W0
 -- >>> toShifted [T,T,T,T] 5
--- Shifted 1 7 "\128"
+-- Shifted 1 7 (W1 128)
 
 toShifted :: Bits -> Int -> Shifted
-toShifted bits n = Shifted r w bs
+toShifted bits n = Shifted r w ws
   where
     shifted = replicate n F ++ bits
     len = length shifted
     !r = len `mod` 8
-    bs0 = BS.pack $ map fromBits $ group8 shifted
-    (!w,!bs) = (BS.head bs0, BS.tail bs0)
+    ws0 = map fromBits $ group8 shifted
+    !w = head ws0
+    !ws = case tail ws0 of
+        []            -> W0
+        [w1]          -> W1 w1
+        [w1,w2]       -> W2 w1 w2
+        [w1,w2,w3]    -> W3 w1 w2 w3
+        [w1,w2,w3,w4] -> W4 w1 w2 w3 w4
+        _             -> error "toShifted"
     group8 xs
       | null zs   = pad ys : []
       | otherwise = ys : group8 zs
@@ -86,7 +99,7 @@ enc dst rbuf = returnLength dst $ go 0
               else do
                 b0 <- readWord8 dst
                 writeWord8 dst (b0 .|. b)
-            copyByteString dst bs
+            copy dst bs
             when (n' /= 0) $ wind dst (-1)
             go n'
           else
@@ -94,6 +107,23 @@ enc dst rbuf = returnLength dst $ go 0
                 let Shifted _ b _ = (aosa ! idxEos) ! n
                 b0 <- readWord8 dst
                 writeWord8 dst (b0 .|. b)
+
+{-# INLINE copy #-}
+copy :: WorkingBuffer -> WS -> IO ()
+copy _ W0 = return ()
+copy dst (W1 w1) = writeWord8 dst w1
+copy dst (W2 w1 w2) = do
+    writeWord8 dst w1
+    writeWord8 dst w2
+copy dst (W3 w1 w2 w3) = do
+    writeWord8 dst w1
+    writeWord8 dst w2
+    writeWord8 dst w3
+copy dst (W4 w1 w2 w3 w4) = do
+    writeWord8 dst w1
+    writeWord8 dst w2
+    writeWord8 dst w3
+    writeWord8 dst w4
 
 encodeHuffman :: ByteString -> IO ByteString
 encodeHuffman bs = withTemporaryBuffer 4096 $ \wbuf ->
