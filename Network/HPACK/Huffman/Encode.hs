0fcf2c6097b276d8aed908e92e5605ab66f98691
Kazu Yamamoto <kazu@iij.ad.jp>
2016-08-15 11:00:09 +0900

    Huffman encoding from H2O.

--- Network/HPACK/Huffman/Encode.hs
+++ Network/HPACK/Huffman/Encode.hs
@@ -12,78 +12,27 @@ import Control.Applicative ((<$>))
 #endif
 import Control.Exception (throwIO)
 import Control.Monad (when, void)
-import Data.Array (Array, listArray)
 import Data.Array.Base (unsafeAt)
-import Data.Bits ((.|.))
+import Data.Array.IArray (listArray)
+import Data.Array.Unboxed (UArray)
+import Data.Bits ((.|.), shiftR, shiftL)
 import Data.ByteString (ByteString)
 import Data.IORef
-import Data.Word (Word8)
-import Foreign.Ptr (plusPtr, minusPtr, Ptr)
-import Foreign.Storable (peek, poke)
+import Data.Word (Word8, Word64)
+import Foreign.Ptr (plusPtr, minusPtr)
+import Foreign.Storable (poke)
 import Network.HPACK.Buffer
-import Network.HPACK.Huffman.Bit
-import Network.HPACK.Huffman.Params
+import Network.HPACK.Huffman.Params (idxEos)
 import Network.HPACK.Huffman.Table
 import Network.HPACK.Types (BufferOverrun(..))
 
 ----------------------------------------------------------------
 
-type AOSA = Array Int ShiftedArray
+huffmanLength :: UArray Int Int
+huffmanLength = listArray (0,idxEos) $ map length huffmanTable
 
-type ShiftedArray = Array Int Shifted
-
-data WS = W0
-        | W1 !Word8
-        | W2 !Word8 !Word8
-        | W3 !Word8 !Word8 !Word8
-        | W4 !Word8 !Word8 !Word8 !Word8
-        deriving Show
-
-data Shifted = Shifted !Int   -- How many bits in the last byte
-                       !Int   -- Total bytes (3rd Word8 + 4th WS)
-                       !Word8 -- First word.
-                       !WS    -- Following words, up to 4 bytes
-                       deriving Show
-
-----------------------------------------------------------------
-
-aosa :: AOSA
-aosa = listArray (0,idxEos) $ map toShiftedArray huffmanTable
-
--- |
---
--- >>> toShifted [T,T,T,T] 0
--- Shifted 4 1 240 W0
--- >>> toShifted [T,T,T,T] 4
--- Shifted 0 1 15 W0
--- >>> toShifted [T,T,T,T] 5
--- Shifted 1 2 7 (W1 128)
-
-toShifted :: Bits -> Int -> Shifted
-toShifted bits n = Shifted r siz w ws
-  where
-    shifted = replicate n F ++ bits
-    len = length shifted
-    !r = len `mod` 8
-    ws0 = map fromBits $ group8 shifted
-    !siz = length ws0
-    !w = head ws0
-    !ws = case tail ws0 of
-        []            -> W0
-        [w1]          -> W1 w1
-        [w1,w2]       -> W2 w1 w2
-        [w1,w2,w3]    -> W3 w1 w2 w3
-        [w1,w2,w3,w4] -> W4 w1 w2 w3 w4
-        _             -> error "toShifted"
-    group8 xs
-      | null zs   = pad ys : []
-      | otherwise = ys : group8 zs
-      where
-        (ys,zs) = splitAt 8 xs
-    pad xs = take 8 $ xs ++ repeat F
-
-toShiftedArray :: Bits -> ShiftedArray
-toShiftedArray bits = listArray (0,7) $ map (toShifted bits) [0..7]
+huffmanCode :: UArray Int Word64
+huffmanCode = listArray (0,idxEos) huffmanTable'
 
 ----------------------------------------------------------------
 
@@ -94,54 +43,55 @@ type HuffmanEncoding = WorkingBuffer -> ByteString -> IO Int
 encode :: HuffmanEncoding
 encode dst bs = withReadBuffer bs $ enc dst
 
+-- The maximum length of Huffman code is 30.
+-- 40 is enough as a work space.
+initialOffset :: Int
+initialOffset = 40
+
+shiftForWrite :: Int
+shiftForWrite = 32
+
 enc :: WorkingBuffer -> ReadBuffer -> IO Int
 enc WorkingBuffer{..} rbuf = do
     beg <- readIORef offset
-    end <- go 0 beg
+    end <- go (beg,0,initialOffset)
     writeIORef offset end
     let !len = end `minusPtr` beg
     return len
   where
-    go n ptr = do
+    go (dst,encoded,off) = do
         !i <- getByte' rbuf
-        if i >= 0 then do
-            let Shifted n' len w ws = (aosa `unsafeAt` i) `unsafeAt` n
-                !ptr' | n' == 0   = ptr `plusPtr` len
-                      | otherwise = ptr `plusPtr` (len - 1)
-            when (ptr' >= limit) $ throwIO BufferOverrun
-            if n == 0 then
-                poke ptr w
-              else do
-                w0 <- peek ptr
-                poke ptr (w0 .|. w)
-            copy (ptr `plusPtr` 1) ws
-            go n' ptr'
-          else
-            if (n == 0) then
-                return ptr
-              else do
-                let Shifted _ _ w _ = (aosa `unsafeAt` idxEos) `unsafeAt` n
-                w0 <- peek ptr
-                poke ptr (w0 .|. w)
-                let !ptr' = ptr `plusPtr` 1
-                return ptr'
-
-{-# INLINE copy #-}
-copy :: Ptr Word8 -> WS -> IO ()
-copy _ W0 = return ()
-copy ptr (W1 w1) = poke ptr w1
-copy ptr (W2 w1 w2) = do
-    poke ptr w1
-    poke (ptr `plusPtr` 1) w2
-copy ptr (W3 w1 w2 w3) = do
-    poke ptr w1
-    poke (ptr `plusPtr` 1) w2
-    poke (ptr `plusPtr` 2) w3
-copy ptr (W4 w1 w2 w3 w4) = do
-    poke ptr w1
-    poke (ptr `plusPtr` 1) w2
-    poke (ptr `plusPtr` 2) w3
-    poke (ptr `plusPtr` 3) w4
+        if i >= 0 then
+            copy dst (bond i) >>= go
+          else if off == initialOffset then
+            return dst
+          else do
+            let (encoded1,_) = bond idxEos
+            write dst encoded1
+      where
+        {-# INLINE bond #-}
+        bond i = (encoded', off')
+          where
+            !len = huffmanLength `unsafeAt` i
+            !code = huffmanCode `unsafeAt` i
+            !scode = code `shiftL` (off - len)
+            !encoded' = encoded .|. scode
+            !off' = off - len
+        {-# INLINE write #-}
+        write p w = do
+            when (p >= limit) $ throwIO BufferOverrun
+            let !w8 = fromIntegral (w `shiftR` shiftForWrite) :: Word8
+            poke p w8
+            let !p' = p `plusPtr` 1
+            return p'
+        {-# INLINE copy #-}
+        copy p (w,o)
+          | o > shiftForWrite = return (p,w,o)
+          | otherwise = do
+              p' <- write p w
+              let !w' = w `shiftL` 8
+                  !o' = o + 8
+              copy p' (w',o')
 
 encodeHuffman :: ByteString -> IO ByteString
 encodeHuffman bs = withTemporaryBuffer 4096 $ \wbuf ->
