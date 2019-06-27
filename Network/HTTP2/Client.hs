399fe6f15af834c8c06e121938ee9de63e7acaec
Kazu Yamamoto <kazu@iij.ad.jp>
2019-06-27 10:46:31 +0900

    introducing client's Context.

--- Network/HTTP2/Client.hs
+++ Network/HTTP2/Client.hs
@@ -2,8 +2,7 @@
 
 module Network.HTTP2.Client (
     openHTTP2Connection
-  , sendFrame
-  , recvFrame
+  , Context(..)
   ) where
 
 import Control.Monad (void)
@@ -14,21 +13,29 @@ import Network.HTTP2
 import Network.Socket
 import qualified Network.Socket.ByteString as NSB
 
+data Context = Context {
+    sendFrame    :: (FrameFlags -> FrameFlags) -> Int -> FramePayload -> IO ()
+  , recvFrame    :: IO Frame
+  , encodeHeader :: HeaderList -> IO ByteString
+  , decodeHeader :: ByteString -> IO HeaderList
+  }
+
 ----------------------------------------------------------------
 
-openHTTP2Connection :: Socket -> IO (HeaderList -> IO ByteString
-                                    ,ByteString -> IO HeaderList)
+openHTTP2Connection :: Socket -> IO Context
 openHTTP2Connection sock = do
     etbl <- HPACK.newDynamicTableForEncoding HPACK.defaultDynamicTableSize
     dtbl <- HPACK.newDynamicTableForDecoding HPACK.defaultDynamicTableSize 4096
     NSB.sendAll sock connectionPreface
-    sendFrame sock id 0 initialSettingFrame
-    void $ recvFrame sock
-    void $ recvFrame sock
-    sendFrame sock setAck 0 ackSettingsFrame
+    sendFrame' sock id 0 initialSettingFrame
+    void $ recvFrame' sock
+    void $ recvFrame' sock
+    sendFrame' sock setAck 0 ackSettingsFrame
     let enc = HPACK.encodeHeader HPACK.defaultEncodeStrategy 4096 etbl
         dec = HPACK.decodeHeader dtbl
-    return (enc, dec)
+        send = sendFrame' sock
+        recv = recvFrame' sock
+    return $ Context send recv enc dec
 
 initialSettingFrame :: FramePayload
 initialSettingFrame = SettingsFrame [
@@ -40,14 +47,14 @@ ackSettingsFrame = SettingsFrame []
 
 ----------------------------------------------------------------
 
-sendFrame :: Socket -> (FrameFlags -> FrameFlags) -> Int -> FramePayload -> IO ()
-sendFrame sock func sid payload = do
+sendFrame' :: Socket -> (FrameFlags -> FrameFlags) -> Int -> FramePayload -> IO ()
+sendFrame' sock func sid payload = do
     let einfo = encodeInfo func sid
         frame = encodeFrame einfo payload
     NSB.sendAll sock frame
 
-recvFrame :: Socket -> IO Frame
-recvFrame sock = do
+recvFrame' :: Socket -> IO Frame
+recvFrame' sock = do
     (frameId, header) <- decodeFrameHeader <$> NSB.recv sock frameHeaderLength
     let len = payloadLength header
     body <- if len == 0 then return "" else NSB.recv sock len
