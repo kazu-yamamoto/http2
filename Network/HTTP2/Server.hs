ed3f7260fabe9895f756d49fc6bd04312bbfa20b
Kazu Yamamoto <kazu@iij.ad.jp>
2019-08-14 11:02:18 +0900

    using network-run for the server example.

--- Network/HTTP2/Server.hs
+++ Network/HTTP2/Server.hs
@@ -8,17 +8,15 @@
 -- > {-# LANGUAGE OverloadedStrings #-}
 -- > module Main (main) where
 -- >
--- > import Control.Concurrent (forkFinally)
--- > import Control.Exception (SomeException(..))
 -- > import qualified Control.Exception as E
--- > import Control.Monad (forever, void)
 -- > import Data.ByteString.Builder (byteString)
 -- > import Network.HTTP.Types (ok200)
+-- > import Network.Run.TCP (runTCPServer) -- network-run
+-- >
 -- > import Network.HTTP2.Server
--- > import Network.Socket
 -- >
 -- > main :: IO ()
--- > main = runTCPServer "80" $ \s _peer -> runHTTP2Server s
+-- > main = runTCPServer Nothing "80" $ \s _peer -> runHTTP2Server s
 -- >   where
 -- >     runHTTP2Server s = E.bracket (allocSimpleConfig s 4096)
 -- >                                  (\config -> run config server)
@@ -28,31 +26,6 @@
 -- >         response = responseBuilder ok200 header body
 -- >         header = [("Content-Type", "text/plain")]
 -- >         body = byteString "Hello, world!\n"
--- >
--- > runTCPServer :: String -> (Socket -> SockAddr -> IO a) -> IO a
--- > runTCPServer port server = withSocketsDo $ do
--- >     addr <- resolve
--- >     E.bracket (open addr) close loop
--- >   where
--- >     resolve = do
--- >         let hints = defaultHints {
--- >                 addrFlags = [AI_PASSIVE]
--- >               , addrSocketType = Stream
--- >               }
--- >         head <$> getAddrInfo (Just hints) Nothing (Just port)
--- >     open addr = do
--- >         sock <- socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
--- >         setSocketOption sock ReuseAddr 1
--- >         withFdSocket sock $ setCloseOnExecIfNeeded
--- >         bind sock $ addrAddress addr
--- >         listen sock 1024
--- >         return sock
--- >     loop sock = forever $ do
--- >         (conn, peer) <- accept sock
--- >         void $ forkFinally (server conn peer) (clear conn)
--- >     clear conn _ = shutdown conn ShutdownSend `E.catch` ignore
--- >       where
--- >         ignore (SomeException _) = return ()
 
 module Network.HTTP2.Server (
   -- * Runner
