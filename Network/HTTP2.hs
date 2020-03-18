8b283f2136c6b90eca95047e6abaa8220ba71652
Kazu Yamamoto <kazu@iij.ad.jp>
2020-03-18 13:51:53 +0900

    deprecating Network.HTTP2

--- Network/HTTP2.hs
+++ Network/HTTP2.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 -- | Framing in HTTP\/2(<https://tools.ietf.org/html/rfc7540>).
-module Network.HTTP2 (
+module Network.HTTP2 {-# DEPRECATED "Use Network.HTTP2.Frame instead" #-}
+  (
     module Network.HTTP2.Frame
   ) where
 
