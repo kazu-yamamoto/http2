c80a178580e0823c7364e42744c4e4af78bdc3ae
Kazu Yamamoto <kazu@iij.ad.jp>
2020-04-29 11:52:32 +0900

    improving Internal modules.

--- Network/HTTP2/Internal.hs
+++ Network/HTTP2/Internal.hs
@@ -1,17 +1,30 @@
 module Network.HTTP2.Internal (
   -- * File
     module Network.HTTP2.Arch.File
-  -- * Manager
-  , module Network.HTTP2.Arch.Manager
   -- * Types
-  , module Network.HTTP2.Arch.Types
+  , Scheme
+  , Authority
+  , Path
+  -- * Request and response
+  , InpObj(..)
+  , InpBody
+  , OutObj(..)
+  , OutBody(..)
+  , FileSpec(..)
+  -- * Trailer
+  , TrailersMaker
+  , defaultTrailersMaker
+  , NextTrailersMaker(..)
   -- * Sender
+  , Next(..)
+  , BytesFilled
+  , DynaNext
+  , RspStreaming(..)
   , fillBuilderBodyGetNext
   , fillFileBodyGetNext
   , fillStreamBodyGetNext
   )  where
 
 import Network.HTTP2.Arch.File
-import Network.HTTP2.Arch.Manager
 import Network.HTTP2.Arch.Types
 import Network.HTTP2.Arch.Sender
--- Network/HTTP2/Server/Internal.hs
+++ Network/HTTP2/Server/Internal.hs
@@ -1,8 +1,6 @@
 module Network.HTTP2.Server.Internal (
     Request(..)
   , Response(..)
-  , module Network.HTTP2.Server.Worker
   ) where
 
 import Network.HTTP2.Server.Types
-import Network.HTTP2.Server.Worker
