2a4135e9be336a8ae5664d8895e5f7b34982026e
Kazu Yamamoto <kazu@iij.ad.jp>
2021-01-20 10:43:10 +0900

    removing an unnecessary error.

--- Network/HPACK/Types.hs
+++ Network/HPACK/Types.hs
@@ -89,7 +89,6 @@ data DecodeError = IndexOverrun Index -- ^ Index is out of range
                  | EosInTheMiddle -- ^ Eos appears in the middle of huffman string
                  | IllegalEos -- ^ Non-eos appears in the end of huffman string
                  | TooLongEos -- ^ Eos of huffman string is more than 7 bits
-                 | EmptyEncodedString -- ^ Encoded string has no length
                  | TooSmallTableSize -- ^ A peer set the dynamic table size less than 32
                  | TooLargeTableSize -- ^ A peer tried to change the dynamic table size over the limit
                  | IllegalTableSizeUpdate -- ^ Table size update at the non-beginning
