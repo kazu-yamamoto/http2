0fdc5b6f02b348047e9136ef21deaf8277eab6dc
Kazu Yamamoto <kazu@iij.ad.jp>
2022-06-24 14:21:17 +0900

    updating example

--- Network/HTTP2/Client.hs
+++ Network/HTTP2/Client.hs
@@ -5,10 +5,11 @@
 --  Example:
 --
 -- > {-# LANGUAGE OverloadedStrings #-}
+-- >
 -- > module Main where
 -- >
+-- > import Control.Concurrent.Async
 -- > import qualified Control.Exception as E
--- > import Control.Concurrent (forkIO, threadDelay)
 -- > import qualified Data.ByteString.Char8 as C8
 -- > import Network.HTTP.Types
 -- > import Network.Run.TCP (runTCPClient) -- network-run
@@ -19,21 +20,25 @@
 -- > serverName = "127.0.0.1"
 -- >
 -- > main :: IO ()
--- > main = runTCPClient serverName "80" runHTTP2Client
+-- > main = runTCPClient serverName "80" $ runHTTP2Client serverName
 -- >   where
--- >     cliconf = ClientConfig "http" (C8.pack serverName) 20
--- >     runHTTP2Client s = E.bracket (allocSimpleConfig s 4096)
--- >                                  freeSimpleConfig
--- >                                  (\conf -> run cliconf conf client)
+-- >     cliconf host = ClientConfig "http" (C8.pack host) 20
+-- >     runHTTP2Client host s = E.bracket (allocSimpleConfig s 4096)
+-- >                                       freeSimpleConfig
+-- >                                       (\conf -> run (cliconf host) conf client)
 -- >     client sendRequest = do
--- >         let req = requestNoBody methodGet "/" []
--- >         _ <- forkIO $ sendRequest req $ \rsp -> do
--- >             print rsp
--- >             getResponseBodyChunk rsp >>= C8.putStrLn
--- >         sendRequest req $ \rsp -> do
--- >             threadDelay 100000
--- >             print rsp
--- >             getResponseBodyChunk rsp >>= C8.putStrLn
+-- >         let req0 = requestNoBody methodGet "/" []
+-- >             client0 = sendRequest req0 $ \rsp -> do
+-- >                 print rsp
+-- >                 getResponseBodyChunk rsp >>= C8.putStrLn
+-- >             req1 = requestNoBody methodGet "/foo" []
+-- >             client1 = sendRequest req1 $ \rsp -> do
+-- >                 print rsp
+-- >                 getResponseBodyChunk rsp >>= C8.putStrLn
+-- >         ex <- E.try $ concurrently_ client0 client1
+-- >         case ex of
+-- >           Left  e  -> print (e :: HTTP2Error)
+-- >           Right () -> putStrLn "OK"
 
 module Network.HTTP2.Client (
   -- * Runner
