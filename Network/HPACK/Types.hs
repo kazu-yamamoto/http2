18699dc38164a169b4cae067df08c9c14236d23c
Kazu Yamamoto <kazu@iij.ad.jp>
2023-10-27 16:37:00 +0900

    fourmolu

--- Network/HPACK/Types.hs
+++ Network/HPACK/Types.hs
@@ -1,30 +1,33 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 
 module Network.HPACK.Types (
-  -- * Header
-    HeaderName
-  , HeaderValue
-  , Header
-  , HeaderList
-  , TokenHeader
-  , TokenHeaderList
-  -- * Misc
-  , Index
-  , HIndex(..)
-  -- * Encoding and decoding
-  , CompressionAlgo(..)
-  , EncodeStrategy(..)
-  , defaultEncodeStrategy
-  , DecodeError(..)
-  -- * Buffer
-  , Buffer
-  , BufferSize
-  , BufferOverrun(..)
-  ) where
+    -- * Header
+    HeaderName,
+    HeaderValue,
+    Header,
+    HeaderList,
+    TokenHeader,
+    TokenHeaderList,
+
+    -- * Misc
+    Index,
+    HIndex (..),
+
+    -- * Encoding and decoding
+    CompressionAlgo (..),
+    EncodeStrategy (..),
+    defaultEncodeStrategy,
+    DecodeError (..),
+
+    -- * Buffer
+    Buffer,
+    BufferSize,
+    BufferOverrun (..),
+) where
 
 import Control.Exception as E
 import Data.Typeable
-import Network.ByteOrder (Buffer, BufferSize, BufferOverrun(..))
+import Network.ByteOrder (Buffer, BufferOverrun (..), BufferSize)
 
 import Imports
 import Network.HPACK.Token (Token)
@@ -59,42 +62,56 @@ data HIndex = SIndex Int | DIndex Int deriving (Eq, Ord, Show)
 ----------------------------------------------------------------
 
 -- | Compression algorithms for HPACK encoding.
-data CompressionAlgo = Naive  -- ^ No compression
-                     | Static -- ^ Using indices in the static table only
-                     | Linear -- ^ Using indices
-                     deriving (Eq, Show)
+data CompressionAlgo
+    = -- | No compression
+      Naive
+    | -- | Using indices in the static table only
+      Static
+    | -- | Using indices
+      Linear
+    deriving (Eq, Show)
 
 -- | Strategy for HPACK encoding.
-data EncodeStrategy = EncodeStrategy {
-  -- | Which compression algorithm is used.
-    compressionAlgo :: CompressionAlgo
-  -- | Whether or not to use Huffman encoding for strings.
-  , useHuffman :: Bool
-  } deriving (Eq, Show)
+data EncodeStrategy = EncodeStrategy
+    { compressionAlgo :: CompressionAlgo
+    -- ^ Which compression algorithm is used.
+    , useHuffman :: Bool
+    -- ^ Whether or not to use Huffman encoding for strings.
+    }
+    deriving (Eq, Show)
 
 -- | Default 'EncodeStrategy'.
 --
 -- >>> defaultEncodeStrategy
 -- EncodeStrategy {compressionAlgo = Linear, useHuffman = False}
 defaultEncodeStrategy :: EncodeStrategy
-defaultEncodeStrategy = EncodeStrategy {
-    compressionAlgo = Linear
-  , useHuffman = False
-  }
+defaultEncodeStrategy =
+    EncodeStrategy
+        { compressionAlgo = Linear
+        , useHuffman = False
+        }
 
 ----------------------------------------------------------------
 
 -- | Errors for decoder.
-data DecodeError = IndexOverrun Index -- ^ Index is out of range
-                 | EosInTheMiddle -- ^ Eos appears in the middle of huffman string
-                 | IllegalEos -- ^ Non-eos appears in the end of huffman string
-                 | TooLongEos -- ^ Eos of huffman string is more than 7 bits
-                 | TooSmallTableSize -- ^ A peer set the dynamic table size less than 32
-                 | TooLargeTableSize -- ^ A peer tried to change the dynamic table size over the limit
-                 | IllegalTableSizeUpdate -- ^ Table size update at the non-beginning
-                 | HeaderBlockTruncated
-                 | IllegalHeaderName
-                 | TooLargeHeader
-                 deriving (Eq,Show,Typeable)
+data DecodeError
+    = -- | Index is out of range
+      IndexOverrun Index
+    | -- | Eos appears in the middle of huffman string
+      EosInTheMiddle
+    | -- | Non-eos appears in the end of huffman string
+      IllegalEos
+    | -- | Eos of huffman string is more than 7 bits
+      TooLongEos
+    | -- | A peer set the dynamic table size less than 32
+      TooSmallTableSize
+    | -- | A peer tried to change the dynamic table size over the limit
+      TooLargeTableSize
+    | -- | Table size update at the non-beginning
+      IllegalTableSizeUpdate
+    | HeaderBlockTruncated
+    | IllegalHeaderName
+    | TooLargeHeader
+    deriving (Eq, Show, Typeable)
 
 instance Exception DecodeError
