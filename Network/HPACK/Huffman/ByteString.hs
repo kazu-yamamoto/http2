322a2f6ae49918a3874b8acc0ff6ca5adc952509
Kazu Yamamoto <kazu@iij.ad.jp>
2023-10-27 16:13:01 +0900

    fourmolu

--- Network/HPACK/Huffman/ByteString.hs
+++ Network/HPACK/Huffman/ByteString.hs
@@ -1,11 +1,11 @@
 {-# LANGUAGE ForeignFunctionInterface #-}
 
 module Network.HPACK.Huffman.ByteString (
-    unpack4bits
-  , copy
-  ) where
+    unpack4bits,
+    copy,
+) where
 
-import Foreign.C.Types (CSize(..))
+import Foreign.C.Types (CSize (..))
 import Foreign.Ptr (Ptr, plusPtr)
 import Foreign.Storable (peek)
 import System.IO.Unsafe (unsafeDupablePerformIO)
@@ -22,30 +22,29 @@ import Imports
 -- [1,2,3,4,15,3,10,11]
 -- >>> unpack4bits $ BS.tail bs
 -- [3,4,15,3,10,11]
-
 unpack4bits :: ByteString -> [Word8]
 unpack4bits (PS fptr off len) = unsafeDupablePerformIO $
-  withForeignPtr fptr $ \ptr -> do
-    let lim = ptr `plusPtr` (off - 1)
-        end = ptr `plusPtr` (off + len - 1)
-    go lim end []
+    withForeignPtr fptr $ \ptr -> do
+        let lim = ptr `plusPtr` (off - 1)
+            end = ptr `plusPtr` (off + len - 1)
+        go lim end []
   where
     go lim p ws
-      | lim == p = return ws
-      | otherwise = do
-          w <- peek p
-          let w0 = w `shiftR` 4
-              w1 = w .&. 0xf
-          go lim (p `plusPtr` (-1)) (w0:w1:ws)
-
+        | lim == p = return ws
+        | otherwise = do
+            w <- peek p
+            let w0 = w `shiftR` 4
+                w1 = w .&. 0xf
+            go lim (p `plusPtr` (-1)) (w0 : w1 : ws)
 
 copy :: Ptr Word8 -> ByteString -> IO ()
 copy dst (PS fptr off len) = withForeignPtr fptr $ \ptr -> do
     let beg = ptr `plusPtr` off
     memcpy dst beg (fromIntegral len)
 
-foreign import ccall unsafe "string.h memcpy" c_memcpy
-    :: Ptr Word8 -> Ptr Word8 -> CSize -> IO (Ptr Word8)
+foreign import ccall unsafe "string.h memcpy"
+    c_memcpy
+        :: Ptr Word8 -> Ptr Word8 -> CSize -> IO (Ptr Word8)
 
 memcpy :: Ptr Word8 -> Ptr Word8 -> Int -> IO ()
 memcpy dst src s = void $ c_memcpy dst src (fromIntegral s)
--- Network/HPACK/Huffman/Encode.hs
+++ Network/HPACK/Huffman/Encode.hs
@@ -1,17 +1,17 @@
 {-# LANGUAGE RecordWildCards #-}
 
 module Network.HPACK.Huffman.Encode (
-  -- * Huffman encoding
-    encodeH
-  , encodeHuffman
-  ) where
+    -- * Huffman encoding
+    encodeH,
+    encodeHuffman,
+) where
 
 import Control.Exception (throwIO)
 import Data.Array.Base (unsafeAt)
 import Data.Array.IArray (listArray)
 import Data.Array.Unboxed (UArray)
 import Data.IORef
-import Foreign.Ptr (plusPtr, minusPtr)
+import Foreign.Ptr (minusPtr, plusPtr)
 import Foreign.Storable (poke)
 import Network.ByteOrder hiding (copy)
 
@@ -22,17 +22,20 @@ import Network.HPACK.Huffman.Table
 ----------------------------------------------------------------
 
 huffmanLength :: UArray Int Int
-huffmanLength = listArray (0,idxEos) $ map length huffmanTable
+huffmanLength = listArray (0, idxEos) $ map length huffmanTable
 
 huffmanCode :: UArray Int Word64
-huffmanCode = listArray (0,idxEos) huffmanTable'
+huffmanCode = listArray (0, idxEos) huffmanTable'
 
 ----------------------------------------------------------------
 
 -- | Huffman encoding.
-encodeH :: WriteBuffer
-        -> ByteString -- ^ Target
-        -> IO Int     -- ^ The length of the encoded string.
+encodeH
+    :: WriteBuffer
+    -> ByteString
+    -- ^ Target
+    -> IO Int
+    -- ^ The length of the encoded string.
 encodeH dst bs = withReadBuffer bs $ enc dst
 
 -- The maximum length of Huffman code is 30.
@@ -46,20 +49,21 @@ shiftForWrite = 32
 enc :: WriteBuffer -> ReadBuffer -> IO Int
 enc WriteBuffer{..} rbuf = do
     beg <- readIORef offset
-    end <- go (beg,0,initialOffset)
+    end <- go (beg, 0, initialOffset)
     writeIORef offset end
     let len = end `minusPtr` beg
     return len
   where
-    go (dst,encoded,off) = do
+    go (dst, encoded, off) = do
         i <- readInt8 rbuf
-        if i >= 0 then
-            cpy dst (bond i) >>= go
-          else if off == initialOffset then
-            return dst
-          else do
-            let (encoded1,_) = bond idxEos
-            write dst encoded1
+        if i >= 0
+            then cpy dst (bond i) >>= go
+            else
+                if off == initialOffset
+                    then return dst
+                    else do
+                        let (encoded1, _) = bond idxEos
+                        write dst encoded1
       where
         {-# INLINE bond #-}
         bond i = (encoded', off')
@@ -77,13 +81,13 @@ enc WriteBuffer{..} rbuf = do
             let p' = p `plusPtr` 1
             return p'
         {-# INLINE cpy #-}
-        cpy p (w,o)
-          | o > shiftForWrite = return (p,w,o)
-          | otherwise = do
-              p' <- write p w
-              let w' = w `shiftL` 8
-                  o' = o + 8
-              cpy p' (w',o')
+        cpy p (w, o)
+            | o > shiftForWrite = return (p, w, o)
+            | otherwise = do
+                p' <- write p w
+                let w' = w `shiftL` 8
+                    o' = o + 8
+                cpy p' (w', o')
 
 -- | Huffman encoding with a temporary buffer whose size is 4096.
 encodeHuffman :: ByteString -> IO ByteString
