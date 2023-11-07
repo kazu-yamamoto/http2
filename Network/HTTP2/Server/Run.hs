bfb6d66756f7ef62ee6d9122ee3b299eba000ae1
Kazu Yamamoto <kazu@iij.ad.jp>
2023-11-07 16:40:05 +0900

    defining ServerConfig

--- Network/HTTP2/Server/Run.hs
+++ Network/HTTP2/Server/Run.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
 
@@ -13,23 +14,42 @@ import Network.HTTP2.Server.Worker
 import Network.Socket (SockAddr)
 import UnliftIO.Async (concurrently_)
 
+-- | Server configuration
+data ServerConfig = ServerConfig
+    { numberOfworkers :: Int
+    -- ^ The number of workers
+    , maxConcurrency :: Int
+    }
+
+-- | The number of workers is 3.
+-- This was carefully chosen based on a lot of benchmarks.
+-- If it is 1, we cannot avoid head-of-line blocking.
+-- If it is large, huge memory is consumed and many
+-- context switches happen.
+defaultServerConfig :: ServerConfig
+defaultServerConfig =
+    ServerConfig
+        { numberOfworkers = 3
+        , maxConcurrency = recommendedConcurrency
+        }
+
 ----------------------------------------------------------------
 
 -- | Running HTTP/2 server.
 run :: Config -> Server -> IO ()
-run conf server = do
+run = run' defaultServerConfig
+
+run' :: ServerConfig -> Config -> Server -> IO ()
+run' sconf@ServerConfig{numberOfworkers} conf server = do
     ok <- checkPreface conf
     when ok $ do
-        (ctx, mgr) <- setup conf
+        (ctx, mgr) <- setup sconf conf
         let wc = fromContext ctx
         setAction mgr $ worker wc mgr server
-        -- The number of workers is 3.
-        -- This was carefully chosen based on a lot of benchmarks.
-        -- If it is 1, we cannot avoid head-of-line blocking.
-        -- If it is large, huge memory is consumed and many
-        -- context switches happen.
-        replicateM_ 3 $ spawnAction mgr
+        replicateM_ numberOfworkers $ spawnAction mgr
         runArch conf ctx mgr
+
+----------------------------------------------------------------
 
 data ServerIO = ServerIO
     { sioMySockAddr :: SockAddr
@@ -42,13 +62,14 @@ data ServerIO = ServerIO
 -- | Launching a receiver and a sender without workers.
 -- Any frames can be sent with `sioWriteBytes`.
 runIO
-    :: Config
+    :: ServerConfig
+    -> Config
     -> (ServerIO -> IO (IO ()))
     -> IO ()
-runIO conf@Config{..} action = do
+runIO sconf conf@Config{..} action = do
     ok <- checkPreface conf
     when ok $ do
-        (ctx@Context{..}, mgr) <- setup conf
+        (ctx@Context{..}, mgr) <- setup sconf conf
         let ServerInfo{..} = toServerInfo roleInfo
             get = do
                 Input strm inObj <- atomically $ readTQueue inputQ
@@ -69,11 +90,10 @@ checkPreface conf@Config{..} = do
             return False
         else return True
 
-setup :: Config -> IO (Context, Manager)
-setup Config{..} = do
+setup :: ServerConfig -> Config -> IO (Context, Manager)
+setup _ Config{..} = do
     serverInfo <- newServerInfo
-    -- XXX fixme
-    ctx <- newContext serverInfo 100 confBufferSize confMySockAddr confPeerSockAddr
+    ctx <- newContext serverInfo 0 confBufferSize confMySockAddr confPeerSockAddr
     -- Workers, worker manager and timer manager
     mgr <- start confTimeoutManager
     return (ctx, mgr)
