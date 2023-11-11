f4758fa0b00480b83a806774bcecfb279eddff3d
Edsko de Vries <edsko@well-typed.com>
2023-11-11 11:19:43 +0100

    Test deadlock absence for concurrent client

--- test/HTTP2/ClientSpec.hs
+++ test/HTTP2/ClientSpec.hs
@@ -1,10 +1,12 @@
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
 
 module HTTP2.ClientSpec where
 
 import Control.Concurrent
 import qualified Control.Exception as E
+import Control.Monad
 import Data.ByteString (ByteString)
 import Data.ByteString.Builder (byteString)
 import qualified Data.ByteString.Char8 as C8
@@ -12,6 +14,7 @@ import Network.HTTP.Types
 import Network.Run.TCP
 import System.IO.Unsafe (unsafePerformIO)
 import System.Random
+import System.Timeout (timeout)
 import Test.Hspec
 
 import Network.HTTP2.Client
@@ -32,30 +35,48 @@ spec :: Spec
 spec = do
     describe "client" $ do
         it "receives an error if scheme is missing" $
-            E.bracket (forkIO runServer) killThread $ \_ -> do
+            E.bracket (forkIO $ runServer defaultServer) killThread $ \_ -> do
                 threadDelay 10000
-                runClient "" host' [] `shouldThrow` streamError
+                runClient "" host' (defaultClient []) `shouldThrow` streamError
 
         it "receives an error if authority is missing" $
-            E.bracket (forkIO runServer) killThread $ \_ -> do
+            E.bracket (forkIO $ runServer defaultServer) killThread $ \_ -> do
                 threadDelay 10000
-                runClient "http" "" [] `shouldThrow` streamError
+                runClient "http" "" (defaultClient []) `shouldThrow` streamError
 
         it "receives an error if authority and host are different" $
-            E.bracket (forkIO runServer) killThread $ \_ -> do
+            E.bracket (forkIO $ runServer defaultServer) killThread $ \_ -> do
                 threadDelay 10000
-                runClient "http" host' [("Host", "foo")] `shouldThrow` streamError
+                runClient "http" host' (defaultClient [("Host", "foo")])
+                    `shouldThrow` streamError
 
-runServer :: IO ()
-runServer = runTCPServer (Just host) port runHTTP2Server
+        it "does not deadlock (in concurrent setting)" $
+            E.bracket (forkIO $ runServer irresponsiveServer) killThread $ \_ -> do
+                threadDelay 10000
+                resultVar <- newEmptyMVar
+                runClient "http" "localhost" $ concurrentClient resultVar
+                result <- timeout 1000000 $ takeMVar resultVar
+                case result of
+                    Nothing -> expectationFailure "Exception was not raised"
+                    Just (Left ConnectionIsClosed) -> return ()
+                    Just (Left err) -> expectationFailure $ "Raise unexpected exception " ++ show err
+                    Just (Right ()) -> expectationFailure "Unexpected client termination"
+
+runServer :: S.Server -> IO ()
+runServer server = runTCPServer (Just host) port runHTTP2Server
   where
     runHTTP2Server s =
         E.bracket
             (allocSimpleConfig s 4096)
             freeSimpleConfig
             (`S.run` server)
-server :: S.Server
-server _req _aux sendResponse = sendResponse responseHello []
+
+defaultServer :: S.Server
+defaultServer _req _aux sendResponse = sendResponse responseHello []
+
+irresponsiveServer :: S.Server
+irresponsiveServer _req _aux _sendResponse = do
+    forever $ threadDelay 10000000
 
 responseHello :: S.Response
 responseHello = S.responseBuilder ok200 header body
@@ -63,8 +84,8 @@ responseHello = S.responseBuilder ok200 header body
     header = [("Content-Type", "text/plain")]
     body = byteString "Hello, world!\n"
 
-runClient :: Scheme -> Authority -> RequestHeaders -> IO ()
-runClient sc au hd = runTCPClient host port $ runHTTP2Client
+runClient :: Scheme -> Authority -> Client a -> IO a
+runClient sc au client = runTCPClient host port $ runHTTP2Client
   where
     cliconf = defaultClientConfig{scheme = sc, authority = au}
     runHTTP2Client s =
@@ -74,11 +95,21 @@ runClient sc au hd = runTCPClient host port $ runHTTP2Client
             ( \conf -> run cliconf conf $ \sendRequest ->
                 client sendRequest
             )
-    client sendRequest = do
-        let req = requestNoBody methodGet "/" hd
-        sendRequest req $ \rsp -> do
-            responseStatus rsp `shouldBe` Just ok200
-            fmap statusMessage (responseStatus rsp) `shouldBe` Just "OK"
+
+defaultClient :: RequestHeaders -> Client ()
+defaultClient hd sendRequest = do
+    let req = requestNoBody methodGet "/" hd
+    sendRequest req $ \rsp -> do
+        responseStatus rsp `shouldBe` Just ok200
+        fmap statusMessage (responseStatus rsp) `shouldBe` Just "OK"
+
+concurrentClient :: MVar (Either HTTP2Error ()) -> Client ()
+concurrentClient resultVar sendRequest = do
+    let req = requestNoBody methodGet "/" []
+    void $ forkIO $ do
+        result <- E.try $ sendRequest req $ \_rsp -> return ()
+        putMVar resultVar result
+    threadDelay 10000
 
 streamError :: Selector HTTP2Error
 streamError (StreamErrorIsReceived _ _) = True
