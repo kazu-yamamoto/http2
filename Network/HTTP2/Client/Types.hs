ab20537293d73b9d6959ac2965c6f160cdb94789
Kazu Yamamoto <kazu@iij.ad.jp>
2023-11-24 09:42:42 +0900

    fixing doc

--- Network/HTTP2/Client/Types.hs
+++ Network/HTTP2/Client/Types.hs
@@ -22,5 +22,5 @@ data Aux = Aux
     , auxServerMaxStreams :: IO Int
     -- ^ Getting server's SETTINGS_MAX_CONCURRENT_STREAMS.
     --   If the server does not inform it,
-    --   'concurrentStreams' is used.
+    --   'maxBound' is used.
     }
