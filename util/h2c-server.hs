60200be3d4256f9506f5f8d307189102e58eda07
Kazu Yamamoto <kazu@iij.ad.jp>
2024-04-03 14:06:39 +0900

    extracting Server.hs

--- util/h2c-server.hs
+++ util/h2c-server.hs
@@ -1,24 +1,14 @@
-{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main (main) where
 
 import qualified Control.Exception as E
-import Control.Monad
-import Crypto.Hash (Context, SHA1) -- cryptonite
-import qualified Crypto.Hash as CH
-import Data.ByteString (ByteString)
-import qualified Data.ByteString as B
-import Data.ByteString.Builder (byteString)
-import qualified Data.ByteString.Char8 as C8
-import Network.HPACK
-import Network.HPACK.Token
-import Network.HTTP.Types
-import Network.Run.TCP -- network-run
+import Network.HTTP2.Server
+import Network.Run.TCP
 import System.Environment
 import System.Exit
 
-import Network.HTTP2.Server
+import Server
 
 main :: IO ()
 main = do
@@ -35,41 +25,3 @@ main = do
             (allocSimpleConfig s 4096)
             freeSimpleConfig
             (\conf -> run defaultServerConfig conf server)
-    server req _aux sendResponse = case getHeaderValue tokenMethod vt of
-        Just "GET" -> sendResponse responseHello []
-        Just "POST" -> sendResponse (responseEcho req) []
-        _ -> sendResponse response404 []
-      where
-        (_, vt) = requestHeaders req
-
-responseHello :: Response
-responseHello = responseBuilder ok200 header body
-  where
-    header = [("Content-Type", "text/plain")]
-    body = byteString "Hello, world!\n"
-
-response404 :: Response
-response404 = responseNoBody notFound404 []
-
-responseEcho :: Request -> Response
-responseEcho req = setResponseTrailersMaker h2rsp maker
-  where
-    h2rsp = responseStreaming ok200 header streamingBody
-    header = [("Content-Type", "text/plain")]
-    streamingBody write _flush = loop
-      where
-        loop = do
-            bs <- getRequestBodyChunk req
-            unless (B.null bs) $ do
-                void $ write $ byteString bs
-                loop
-    maker = trailersMaker (CH.hashInit :: Context SHA1)
-
--- Strictness is important for Context.
-trailersMaker :: Context SHA1 -> Maybe ByteString -> IO NextTrailersMaker
-trailersMaker ctx Nothing = return $ Trailers [("X-SHA1", sha1)]
-  where
-    !sha1 = C8.pack $ show $ CH.hashFinalize ctx
-trailersMaker ctx (Just bs) = return $ NextTrailersMaker $ trailersMaker ctx'
-  where
-    !ctx' = CH.hashUpdate ctx bs
