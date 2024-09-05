fcb89f920d707d5330f7b32cf942b2b4edf8952e
Kazu Yamamoto <kazu@iij.ad.jp>
2024-09-05 16:20:21 +0900

    thread-less manager

--- Network/HTTP2/H2/Manager.hs
+++ Network/HTTP2/H2/Manager.hs
@@ -27,14 +27,8 @@ import Imports
 
 ----------------------------------------------------------------
 
-data Command
-    = Stop (MVar ()) (Maybe SomeException)
-    | Add ThreadId
-    | RegisterTimeout ThreadId TimeoutKey
-    | Delete ThreadId
-
 -- | Manager to manage the thread and the timer.
-data Manager = Manager (TQueue Command) (TVar Int)
+data Manager = Manager (TVar ManagedThreads)
 
 data TimeoutHandle
     = ThreadWithTimeout TimeoutKey
@@ -53,42 +47,7 @@ type ManagedThreads = Map ThreadId TimeoutHandle
 --   by 'setAction'. This allows that the action can include
 --   the manager itself.
 start :: IO Manager
-start = do
-    q <- newTQueueIO
-    cnt <- newTVarIO 0
-    void $ forkIO $ do
-        labelMe "H2 thread manager"
-        go q Map.empty
-    return $ Manager q cnt
-  where
-    -- This runs in a separate thread whose ThreadId is not known by anyone
-    -- else, so it cannot be killed by asynchronous exceptions.
-    go :: TQueue Command -> ManagedThreads -> IO ()
-    go q threadMap0 = do
-        x <- atomically $ readTQueue q
-        case x of
-            Stop signalTimeoutsDisabled err -> do
-                -- We first remove all threads from the timeout
-                -- manager, then signal that that is complete, and
-                -- finally kill all threads. This avoids a race
-                -- between the timeout manager and our manager: we
-                -- want to ensure that the exception that gets
-                -- delivered is 'KilledByHttp2ThreadManager', not
-                -- 'TimeoutThread'.
-                forM_ (Map.elems threadMap0) cancelTimeout
-                putMVar signalTimeoutsDisabled ()
-                forM_ (Map.keys threadMap0) $ \tid ->
-                    E.throwTo tid $ KilledByHttp2ThreadManager err
-            Add newtid -> do
-                let threadMap = Map.insert newtid ThreadWithoutTimeout threadMap0
-                go q threadMap
-            Delete oldtid -> do
-                forM_ (Map.lookup oldtid threadMap0) cancelTimeout
-                let threadMap = Map.delete oldtid threadMap0
-                go q threadMap
-            RegisterTimeout tid h -> do
-                let threadMap = Map.insert tid (ThreadWithTimeout h) threadMap0
-                go q threadMap
+start = Manager <$> newTVarIO Map.empty
 
 -- | Stopping the manager.
 --
@@ -97,16 +56,17 @@ start = do
 -- to cleanup in all circumstances. If an exception is caught, it is rethrown
 -- after the cleanup is complete.
 stopAfter :: Manager -> IO a -> (Maybe SomeException -> IO ()) -> IO a
-stopAfter (Manager q _) action cleanup = do
+stopAfter (Manager var) action cleanup = do
     mask $ \unmask -> do
         ma <- try $ unmask action
-        signalTimeoutsDisabled <- newEmptyMVar
-        atomically $
-            writeTQueue q $
-                Stop signalTimeoutsDisabled (either Just (const Nothing) ma)
-        -- This call to takeMVar /will/ eventually succeed, because the Manager
-        -- thread cannot be killed (see comment on 'go' in 'start').
-        takeMVar signalTimeoutsDisabled
+        m <- atomically $ do
+            m0 <- readTVar var
+            writeTVar var Map.empty
+            return m0
+        forM_ (Map.elems m) cancelTimeout
+        let er = either Just (const Nothing) ma
+        forM_ (Map.keys m) $ \tid ->
+            E.throwTo tid $ KilledByHttp2ThreadManager er
         case ma of
             Left err -> cleanup (Just err) >> throwIO err
             Right a -> cleanup Nothing >> return a
@@ -129,12 +89,10 @@ forkManagedUnmask mgr label io =
     void $ mask_ $ forkIOWithUnmask $ \unmask -> E.handle handler $ do
         labelMe label
         addMyId mgr
-        incCounter mgr
         -- We catch the exception and do not rethrow it: we don't want the
         -- exception printed to stderr.
         io unmask `catch` \(_e :: SomeException) -> return ()
         deleteMyId mgr
-        decCounter mgr
   where
     handler (E.SomeException _) = return ()
 
@@ -142,9 +100,9 @@ forkManagedUnmask mgr label io =
 --
 -- This is not part of the public API; see 'forkManaged' instead.
 addMyId :: Manager -> IO ()
-addMyId (Manager q _) = do
+addMyId (Manager var) = do
     tid <- myThreadId
-    atomically $ writeTQueue q $ Add tid
+    atomically $ modifyTVar var $ Map.insert tid ThreadWithoutTimeout
 
 -- | Deleting my thread id from the kill-thread list on stopping.
 --
@@ -152,22 +110,27 @@ addMyId (Manager q _) = do
 -- the manager /before/ the thread terminates (thereby assuming responsibility
 -- for thread cleanup yourself).
 deleteMyId :: Manager -> IO ()
-deleteMyId (Manager q _) = do
+deleteMyId (Manager var) = do
     tid <- myThreadId
-    atomically $ writeTQueue q $ Delete tid
+    atomically $ modifyTVar var $ Map.delete tid
+
+waitCounter0 :: Manager -> IO ()
+waitCounter0 (Manager var) = atomically $ do
+    m <- readTVar var
+    check (Map.size m == 0)
 
 ----------------------------------------------------------------
 
 -- | Killing the IO action of the second argument on timeout.
 timeoutKillThread :: Manager -> (TimeoutKey -> IO a) -> IO a
-timeoutKillThread (Manager q _) action =
+timeoutKillThread (Manager var) action =
     E.bracket register unregister action
   where
     register = do
         tid <- myThreadId
         timmgr <- getSystemTimerManager
         key <- registerTimeout timmgr 30000000 $ void $ forkIO $ killThread tid
-        atomically $ writeTQueue q (RegisterTimeout tid key)
+        atomically $ modifyTVar var $ Map.insert tid $ ThreadWithTimeout key
         return key
     unregister key = do
         timmgr <- getSystemTimerManager
@@ -179,16 +142,3 @@ data KilledByHttp2ThreadManager = KilledByHttp2ThreadManager (Maybe SomeExceptio
 instance Exception KilledByHttp2ThreadManager where
     toException = asyncExceptionToException
     fromException = asyncExceptionFromException
-
-----------------------------------------------------------------
-
-incCounter :: Manager -> IO ()
-incCounter (Manager _ cnt) = atomically $ modifyTVar' cnt (+ 1)
-
-decCounter :: Manager -> IO ()
-decCounter (Manager _ cnt) = atomically $ modifyTVar' cnt (subtract 1)
-
-waitCounter0 :: Manager -> IO ()
-waitCounter0 (Manager _ cnt) = atomically $ do
-    n <- readTVar cnt
-    check (n < 1)
